0addf7d05b33ccb5f82d3770357e6c2770be4cc1
johnmcculloch <john.anthony.mcculloch@gmail.com>
2019-04-16 16:08:11 -0400

    JAMS_BW version 1.25

--- R/assemble_contigs.R
+++ R/assemble_contigs.R
@@ -3,120 +3,147 @@
 #' JAMSalpha function
 #' @export
 
-assemble_contigs<-function(opt=NULL){
+assemble_contigs <- function(opt=NULL){
     #Obtain reads to use for assembly
-    opt<-get_reads(opt=opt)
+    opt <- get_reads(opt=opt)
     save.image(file = opt$projimage)
     #Filter reads if necessary
-    opt<-trim_reads(opt=opt)
+    opt <- trim_reads(opt=opt)
     save.image(file = opt$projimage)
     #Eliminate host reads if applicable
-    opt<-filter_host(opt=opt)
+    opt <- filter_host(opt=opt)
     #Check if reads are in tmpdir
-    if(opt$workdir != opt$sampledir){
-        readsworkdir<-file.path(opt$workdir, "reads")
+    if (opt$workdir != opt$sampledir){
+        readsworkdir <- file.path(opt$workdir, "reads")
     } else {
-        readsworkdir<-opt$readsdir
+        readsworkdir <- opt$readsdir
     }
     save.image(file = opt$projimage)
 
     #Set working directory to workdir
     setwd(opt$workdir)
 
-    #Choose input. Assemble with either NAHS or if not present, with trim. 
-    if((opt$host=="none") || (opt$analysis %in% c("isolate", "isolaternaseq"))){
-        inputreads<-opt$trimreads
+    #Choose input. Assemble with either NAHS or if not present, with trim.
+    if ((opt$host=="none") || (opt$analysis %in% c("isolate", "isolaternaseq"))){
+        inputreads <- opt$trimreads
     } else {
-        inputreads<-opt$nahsreads
+        inputreads <- opt$nahsreads
     }
 
-    if(opt$analysis %in% c("metatranscriptome", "isolaternaseq")){
-        #Disconsider unpaired reads from trimming as an input if RNA.
-        if(length(inputreads) > 2){
-            inputreads<-inputreads[1:2]
-            flog.info("For assembling RNA with SPAdes, unpaired trimmed reads will be not be used.")        
+    if (opt$analysis %in% c("metatranscriptome", "isolaternaseq")){
+        #Disregard unpaired reads from trimming as an input if RNA.
+        if (length(inputreads) > 2){
+            inputreads <- inputreads[1:2]
+            flog.info("For assembling RNA with SPAdes, unpaired trimmed reads will be not be used.")
         }
     }
 
     #Adjust input reads to absolute path
-    inputreads<-file.path(readsworkdir, inputreads)
+    inputreads <- file.path(readsworkdir, inputreads)
 
     #Check the files exist or die.
-    if(!(all(file.exists(inputreads)))){
+    if (!(all(file.exists(inputreads)))){
         flog.info("Could not find input reads to assemble. Aborting now.")
         q()
     }
 
     #Estimate number of reads to tag as deep sequencing
-    numreadsarg<-c("-c", "^@", inputreads[1])
-    estnumreads<-as.numeric(system2('grep', args=numreadsarg, stdout=TRUE))
-    numreads<-estnumreads*length(inputreads)
-    if(numreads > 10000000){
-        opt$deepseq<-TRUE
+    numreadsarg <- c("-c", "^@", inputreads[1])
+    estnumreads <- as.numeric(system2('grep', args=numreadsarg, stdout=TRUE))
+    numreads <- estnumreads * min(length(inputreads), 2)
+    readchars <- as.numeric(system2('cat', args=c(inputreads[1], "|", "head", "-100000", "|", "grep", "-v", "^@", "|", "grep", "-v", "^+", "|", "wc", "-c"), stdout=TRUE))
+    readlines <- as.numeric(system2('cat', args=c(inputreads[1], "|", "head", "-100000", "|", "grep", "-v", "^@", "|", "grep", "-v", "^+", "|", "wc", "-l"), stdout=TRUE))
+    estreadlength <- round ((readchars/readlines),0)
+    estnumbases <- estnumreads * estreadlength
+
+    #If estimated coverage is over 40x consider deep. This is a total guesstimate, but will guide the settings for the assemblers. Assuming most of the microbiota are bacteria and that the median genome size of a bacterium is 4 Mbp.
+    if (opt$analysis %in% c("isolate", "isolaternaseq")){
+        estnumgenomes <- 1
     } else {
-        opt$deepseq<-FALSE
+        #Considering that 50 complete bacterial genomes will make up some 90% of the relative abundance of the sample.
+        estnumgenomes <- 50
+    }
+
+    opt$estcoverage <- round((estnumbases / (estnumgenomes * 4000000)), 0)
+
+    if (opt$estcoverage > 40){
+        opt$deepseq <- TRUE
+    } else {
+        opt$deepseq <- FALSE
     }
 
     #If project type is either isolate or rna, make sure the assembler is spades.
-    opt$assembler<-switch(opt$analysis,  "metagenome" = opt$assembler, "metatranscriptome" = "spades", "isolate" ="spades", "isolaternaseq" = "spades")
-    
+    opt$assembler <- switch(opt$analysis,  "metagenome" = opt$assembler, "metatranscriptome" = "spades", "isolate" = "spades", "isolaternaseq" = "spades")
+
     #If read type is Illumina mate-pair library, then default to SPAdes
-    if(opt$seqtype %in% c("illuminamp")){
-        opt$assembler<-"spades"
+    if (opt$seqtype %in% c("illuminamp")){
+        opt$assembler <- "spades"
     }
 
     #If analysis is metagenome and reads are single end, switch to megahit.
-    if((opt$analysis %in% c("metagenome", "metatranscriptome")) && (length(inputreads) < 2)){
+    if ((opt$analysis %in% c("metagenome", "metatranscriptome")) && (length(inputreads) < 2)){
         flog.info("Single end metagenomic reads can only be assembled with MEGAHIT.")
-        opt$assembler<-"megahit"
+        opt$assembler <- "megahit"
     }
- 
-    membytes90<-(opt$totmembytes * 0.9)
-    assemblercmd<-switch(opt$assembler, "megahit" = "megahit", "spades" = "spades.py")
-    opt$assemblerversion<-system2(assemblercmd, args="--version", stdout = TRUE, stderr = TRUE)
+
+    membytes90 <- (opt$totmembytes * 0.9)
+    assemblercmd <- switch(opt$assembler, "megahit" = "megahit", "spades" = "spades.py")
+    opt$assemblerversion <- system2(assemblercmd, args="--version", stdout = TRUE, stderr = TRUE)
 
     #Build command from options present
-    if(opt$assembler == "megahit"){
+    if (opt$assembler == "megahit"){
         #Build arguments for MEGAHIT
-        if(opt$deepseq == TRUE){
-            presets<-"meta-large"
+        #Take estimated sequencing depth into account
+        #https://github.com/voutcn/megahit/wiki/Assembly-Tips
+        if (opt$estcoverage <= 5){
+            kmin <- "21"
+            kstep <- "8"
+            mincount <- "1"
+        } else if ((opt$estcoverage > 5) & (opt$estcoverage <= 40)){
+            kmin <- "23"
+            kstep <- "10"
+            mincount <- "2"
         } else {
-            presets<-"meta-sensitive"
+            kmin <- "27"
+            kstep <- "12"
+            mincount <- "3"
+        }
+        #Find out if estimated read length is odd or even to get kmax
+        if (as.logical(estreadlength %% 2)){
+            kmax <- min(estreadlength, 255)
+        } else {
+          #Subtract 1 from an even read length
+            kmax <- min((estreadlength - 1), 255)
         }
 
-        commonargs<-c("--presets", presets, "-m", membytes90, "--num-cpu-threads", opt$threads, "-o", paste(opt$prefix, "assembly", sep="_"), "--min-contig-len", "500", "--verbose")
-        readargs<-list("-r", c("-1", "-2"), c("-1", "-2", "-r"))
-        readinput<-as.vector(rbind(readargs[[length(inputreads)]], inputreads))
-        assemblerargs<-c(commonargs, readinput)
-        asscontigs<-file.path(paste(opt$prefix, "assembly", sep="_"), "final.contigs.fa")
-        asslog<-file.path(paste(opt$prefix, "assembly", sep="_"), "log")
+        commonargs <- c("--k-min", kmin, "--k-max", kmax, "--k-step", kstep, "--min-count", mincount, "-m", membytes90, "--num-cpu-threads", opt$threads, "-o", paste(opt$prefix, "assembly", sep="_"), "--min-contig-len", "500", "--verbose")
+        readargs <- list("-r", c("-1", "-2"), c("-1", "-2", "-r"))
+        readinput <- as.vector(rbind(readargs[[length(inputreads)]], inputreads))
+        assemblerargs <- c(commonargs, readinput)
+        asscontigs <- file.path(paste(opt$prefix, "assembly", sep="_"), "final.contigs.fa")
+        asslog <- file.path(paste(opt$prefix, "assembly", sep="_"), "log")
 
     } else if(opt$assembler=="spades"){
         #Build arguments for SPAdes
-        commonargs<-c("-t", opt$threads, "-m", (as.integer(opt$totmembytes/1000000000)), "-o", paste(opt$prefix, "assembly", sep="_"))
-        if(opt$seqtype == "illuminamp"){
-            readargs<-list("-s", c("--hqmp1-1", "--hqmp1-2"), c("--hqmp1-1", "--hqmp1-2", "--hqmp1-s"))
+        commonargs <- c("-t", opt$threads, "-m", (as.integer(opt$totmembytes/1000000000)), "-o", paste(opt$prefix, "assembly", sep="_"))
+        if (opt$seqtype == "illuminamp"){
+            readargs <- list("-s", c("--hqmp1-1", "--hqmp1-2"), c("--hqmp1-1", "--hqmp1-2", "--hqmp1-s"))
         } else {
-            readargs<-list("-s", c("--pe1-1", "--pe1-2"), c("--pe1-1", "--pe1-2", "--pe1-s"))
+            readargs <- list("-s", c("--pe1-1", "--pe1-2"), c("--pe1-1", "--pe1-2", "--pe1-s"))
         }
 
-        readinput<-as.vector(rbind(readargs[[length(inputreads)]], inputreads))
-        if(length(inputreads)>1){
+        readinput <- as.vector(rbind(readargs[[length(inputreads)]], inputreads))
+        if (length(inputreads)>1){
             if(opt$seqtype == "illuminamp"){
                 flog.info("Sequence read input is Illumina High Quality Mate-Pair type.")
-                #readinput<-c("--hqmp1-rf", readinput)            
+                readinput <- c("--hqmp1-rf", readinput)
             } else {
-                readinput<-c("--pe1-fr", readinput)
+                readinput <- c("--pe1-fr", readinput)
             }
         }
 
         if(opt$analysis == "isolate"){
-            otherargs<-c("--cov-cutoff", "auto")
-            #if not too deep, do it carefully
-            if(opt$deepseq == FALSE){
-                flog.info("Using SPAdes --careful mode for assembly.")
-                otherargs<-c("--careful", otherargs)
-            }
+            otherargs <- c("--careful", "--cov-cutoff", "auto")
         } else {
             otherargs<-NULL
         }
--- R/fix_interproscanoutput.R
+++ R/fix_interproscanoutput.R
@@ -7,12 +7,12 @@ fix_interproscanoutput<-function(opt=NULL){
 
     if("iprodir" %in% names(opt)){
         #Get interprojob
-        iprojob<-system2('cat', args=file.path(opt$iprodir, "ipro.job"), stdout = TRUE, stderr = FALSE)
+        opt$iprojob <- system2('cat', args=file.path(opt$iprodir, "ipro.job"), stdout = TRUE, stderr = FALSE)
         #See if job finished
-        iprojobstatus<-system2('sacct', args=c("-j", iprojob), stdout = TRUE)
+        iprojobstatus<-system2('sacct', args=c("-j", opt$iprojob), stdout = TRUE)
         #eliminate header
         iprojobstatus<-iprojobstatus[3:length(iprojobstatus)]
-        ratiojobscomplete<-length(grep("COMPLETED", iprojobstatus))/length(iprojobstatus)
+        ratiojobscomplete <- length(grep("COMPLETED", iprojobstatus))/length(iprojobstatus)
         runningjobs<-length(grep("RUNNING", iprojobstatus))
 
         #Delay if there still are jobs to complete
@@ -21,12 +21,12 @@ fix_interproscanoutput<-function(opt=NULL){
         while ((ratiojobscomplete < 0.9) && nattempt < 50) {
             flog.info("Interproscan analysis of proteome is still incomplete.")
             flog.info(paste("There are", runningjobs, " Interpro jobs running for this sample."))
-            flog.info(paste("The proportion of Interpro jobs complete is currently", ratiojobscomplete))
-            flog.info(paste("Will check again in 5 minutes time. This is attempt", nattempt, "of 50 before giving up."))            
-            Sys.sleep(300)
+            flog.info(paste("The proportion of Interpro jobs complete is currently", round(ratiojobscomplete, 2)))
+            flog.info(paste("Will check again in 10 minutes time. This is attempt", nattempt, "of 50 before giving up."))
+            Sys.sleep(600)
             nattempt <- nattempt + 1
             #See if job finished
-            iprojobstatus<-system2('sacct', args=c("-j", iprojob), stdout = TRUE)
+            iprojobstatus<-system2('sacct', args=c("-j", opt$iprojob), stdout = TRUE)
             #eliminate header
             iprojobstatus<-iprojobstatus[3:length(iprojobstatus)]
             ratiojobscomplete<-length(grep("COMPLETED", iprojobstatus))/length(iprojobstatus)
