9b38c81e6f32e3378cab858ee859f038b9bc49f6
johnmcculloch <john.anthony.mcculloch@gmail.com>
2019-04-26 19:40:05 -0400

    Autodetection od BLAST based analyses

--- R/blast_databases.R
+++ R/blast_databases.R
@@ -3,35 +3,42 @@
 #' JAMSalpha function
 #' @export
 
-blast_databases<-function(opt=NULL, blastanalyses=NULL){
+blast_databases <- function(opt=NULL, blastanalyses=NULL){
+
     #Set working directory to workdir
     setwd(opt$workdir)
-    
+
     #Write predicted proteome to serve as query.
     flog.info("Writing predicted proteome to serve as query.")
     write.fasta(sequences = opt$proteome, names = names(opt$proteome), nbchar = 80, file.out = "proteome.faa")
     flog.info("Writing predicted genes to serve as query.")
     write.fasta(sequences = opt$genes, names = names(opt$genes), nbchar = 80, file.out = "genes.fna")
 
+    if (is.null(blastanalyses)){
+        blastanalyses <- opt$blastanalyses
+    }
+
+    blastanalyses <- blastanalyses[(blastanalyses %in% opt$blastanalyses)]
+
     for(blastanalysis in blastanalyses){
         flog.info(paste("Blasting genes against", blastanalysis, "database"))
 
         #Copy database to tmpdir each time for speed.
-        blastdbori<-file.path(opt$dbdir, "blastdbs", blastanalysis)
-        if(file.exists(blastdbori)){
+        blastdbori <- file.path(opt$blastpath, blastanalysis)
+        if (file.exists(blastdbori)){
             dir.create("tmpblastdb")
             file.copy(file.path(blastdbori, list.files(path=blastdbori, pattern="sequen")), "tmpblastdb")
-            blastdb<-file.path("tmpblastdb", "sequences")
+            blastdb <- file.path("tmpblastdb", "sequences")
 
             #Load lookup table if it exists
             if(file.exists(file.path(blastdbori, "lookup.tsv"))){
-                lookup<-read.table(file=file.path(blastdbori, "lookup.tsv"), sep="\t", header=TRUE, stringsAsFactors=FALSE)
+                lookup <- read.table(file=file.path(blastdbori, "lookup.tsv"), sep="\t", header=TRUE, stringsAsFactors=FALSE)
             } else {
                 data(resfinderlookup)
             }
 
             #Find out if db is protein or DNA
-            if(length(grep("nhr", list.files(blastdbori))) > 0){
+            if (length(grep("nhr", list.files(blastdbori))) > 0){
                 dbtype<-"nucl"
                 blastargs<-c("-num_threads", opt$threads, "-query", "genes.fna", "-db", blastdb, "-outfmt", "'6 qseqid qstart qend qlen sseqid sstart send slen evalue length pident gaps'", "-evalue", "1E-20", "-max_target_seqs", "10000", "-culling_limit", "1", ">", "blast.out")
                 blastprog<-"blastn"
@@ -45,29 +52,29 @@ blast_databases<-function(opt=NULL, blastanalyses=NULL){
             #remove risky characters from hit results
             system("cat blast.out | tr -d \"\'\" > tmp")
             system("mv tmp blast.out")
-            numlinblast<-as.numeric(system2('cat', args=c("blast.out", "|", "wc", "-l"), stdout = TRUE, stderr = FALSE))
-            if(numlinblast > 0){
-                blastout<-read.table(file="blast.out", header=FALSE, sep="\t", colClasses = c("character", "numeric", "numeric", "numeric", "character", "numeric", "numeric", "numeric", "numeric","numeric", "numeric", "numeric"))
-                colnames(blastout)<-c("Feature", "qstart", "qend", "qlen", "Accession", "sstart", "send", "slen", "evalue", "length", "Pident", "gaps")
+            numlinblast <- as.numeric(system2('cat', args=c("blast.out", "|", "wc", "-l"), stdout = TRUE, stderr = FALSE))
+            if (numlinblast > 0){
+                blastout <- read.table(file="blast.out", header=FALSE, sep="\t", colClasses = c("character", "numeric", "numeric", "numeric", "character", "numeric", "numeric", "numeric", "numeric","numeric", "numeric", "numeric"))
+                colnames(blastout) <- c("Feature", "qstart", "qend", "qlen", "Accession", "sstart", "send", "slen", "evalue", "length", "Pident", "gaps")
                 file.remove("blast.out")
 
                 #Cleanup
-                blastout$Qcov<-round(((blastout$qend - blastout$qstart)/blastout$qlen)*100, 2)
-                blastout$Hitcov<-round(((blastout$send - blastout$sstart)/blastout$slen)*100, 2)
-                blastout<-blastout[, c("Feature", "Accession", "Qcov", "Hitcov", "Pident", "gaps", "evalue")]
-                blastout<-subset(blastout, Qcov > 75)
-                blastout<-subset(blastout, Hitcov > 75)
-                blastout<-subset(blastout, Pident > 75)
-                if(nrow(blastout) > 0){
-                    blastout<-left_join(blastout, opt$featuredata)
-                    blastout$LKT<-opt$contigsdata$LKT[match(blastout$Contig, opt$contigsdata$Contig)]
+                blastout$Qcov <- round(((blastout$qend - blastout$qstart)/blastout$qlen)*100, 2)
+                blastout$Hitcov <- round(((blastout$send - blastout$sstart)/blastout$slen)*100, 2)
+                blastout <- blastout[, c("Feature", "Accession", "Qcov", "Hitcov", "Pident", "gaps", "evalue")]
+                blastout <- subset(blastout, Qcov > 75)
+                blastout <- subset(blastout, Hitcov > 75)
+                blastout <- subset(blastout, Pident > 75)
+                if (nrow(blastout) > 0){
+                    blastout <- left_join(blastout, opt$featuredata)
+                    blastout$LKT <- opt$contigsdata$LKT[match(blastout$Contig, opt$contigsdata$Contig)]
                     if(blastanalysis == "resfinder" & !is.null(lookup)){
-                        blastout<-left_join(blastout, lookup, by="Accession")
-                        blastout[is.na(blastout[])]<-"none"
+                        blastout <- left_join(blastout, lookup, by="Accession")
+                        blastout[is.na(blastout[])] <- "none"
                     }
-                    nextopt<-(length(names(opt))+1)
-                    opt[[nextopt]]<-as.data.frame(blastout)
-                    names(opt)[nextopt]<-blastanalysis
+                    nextopt <- (length(names(opt))+1)
+                    opt[[nextopt]] <- as.data.frame(blastout)
+                    names(opt)[nextopt] <- blastanalysis
                } else {
                     flog.info(paste("No significant hits found for", blastanalysis))
                 }
--- R/check_resources.R
+++ R/check_resources.R
@@ -8,11 +8,11 @@ check_resources<-function(opt=opt){
 
     #Check for non-R software dependencies
     deps <- c("pigz", "wget", "sratoolkit", "samtools", "trimmomatic", "bowtie2", "megahit", "spades", "kraken2", "convert2bed", "bedtools")
-    missingdep = FALSE
+    missingdep <- FALSE
     for (dep in deps) {
-        cmd = dep
-        if (cmd=="sratoolkit") cmd = "fastq-dump"
-        if (cmd=="spades") cmd = "spades.py"
+        cmd <- dep
+        if (cmd == "sratoolkit") cmd = "fastq-dump"
+        if (cmd == "spades") cmd = "spades.py"
         if (system(str_c("which ", cmd), ignore.stdout = TRUE)==1) { # not found
             flog.info(str_c("You are missing ", dep))
             missingdep = TRUE
@@ -25,113 +25,125 @@ check_resources<-function(opt=opt){
 
     #Check for kraken2 database
     flog.info("Checking for presence of kraken2 database")
-    k2dbfiles<-list.files(opt$dbdir, recursive=TRUE, full.names=TRUE, include.dirs=FALSE, pattern="*.k2d")
-    if(length(k2dbfiles) != 3){
+    k2dbfiles <- list.files(opt$dbdir, recursive=TRUE, full.names=TRUE, include.dirs=FALSE, pattern="*.k2d")
+    if (length(k2dbfiles) != 3){
         flog.info(paste0("A kraken2 database was not found in", opt$dbdir,". Check your database structure. Aborting."))
-        opt$abort<-TRUE        
+        opt$abort <- TRUE
     }
-    opt$krakendb<-paste0(unlist(strsplit(k2dbfiles[1], split="/"))[1:length(unlist(strsplit(k2dbfiles[1], split="/")))-1], collapse="/")
-    if(!(dir.exists(opt$krakendb))){
+    opt$krakendb <- paste0(unlist(strsplit(k2dbfiles[1], split="/"))[1:length(unlist(strsplit(k2dbfiles[1], split="/")))-1], collapse="/")
+    if (!(dir.exists(opt$krakendb))){
         flog.info("JAMS kraken db directory not found. Aborting.")
-        opt$abort<-TRUE
+        opt$abort <- TRUE
     } else {
         flog.info(paste("JAMS kraken2 database found at", opt$krakendb))
-        krakendbsize<-file.size(file.path(opt$krakendb, "hash.k2d"))
-        if(is.na(krakendbsize)){
+        krakendbsize <- file.size(file.path(opt$krakendb, "hash.k2d"))
+        if (is.na(krakendbsize)){
             flog.info("Kraken2 database supplied seems to be corrupt or not valid. Check your database structure. Aborting.")
-            opt$abort<-TRUE
+            opt$abort <- TRUE
         } else if (krakendbsize > opt$totmembytes) {
             flog.info("It seems there is not enough RAM memory to use the kraken2 database supplied. Try again on a system with more RAM. Aborting.")
-            opt$abort<-TRUE
+            opt$abort <- TRUE
         } else {
-            if(file.exists(file.path(opt$krakendb, "JAMSKdb.ver"))){
-                opt$JAMS_Kdb_Version<-as.character(read.table(file.path(opt$krakendb, "JAMSKdb.ver"), header=FALSE, quote=NULL)[1,1])
+            if (file.exists(file.path(opt$krakendb, "JAMSKdb.ver"))){
+                opt$JAMS_Kdb_Version <- as.character(read.table(file.path(opt$krakendb, "JAMSKdb.ver"), header=FALSE, quote=NULL)[1,1])
             } else {
-                opt$JAMS_Kdb_Version<-"UNDETERMINED"
+                opt$JAMS_Kdb_Version <- "UNDETERMINED"
             }
             flog.info(paste("The JAMS kraken2 database supplied is version", opt$JAMS_Kdb_Version, "and has", round((krakendbsize/1000000000), 1), "Gb."))
         }
     }
 
-    #Set host species to none if isolates or if using contigs.
-    if(opt$analysis=="isolate"){
-        opt$host<-"none"
+    #Check for blast databases
+    blastfiles <- list.files(opt$dbdir, recursive=TRUE, full.names=TRUE, include.dirs=TRUE, pattern="\\.[p|n]hr")
+    if (length(blastfiles) > 0){
+        opt$blastanalyses <- sapply(1:length(blastfiles), function (x) { unlist(strsplit(blastfiles[x], split="/"))[(length(unlist(strsplit(blastfiles[x], split="/"))) - 1)] })
+        opt$blastpath <- paste0(unlist(strsplit(blastfiles[1], split="/"))[1:(length(unlist(strsplit(blastfiles[1], split="/")))-2)], collapse = "/")
+        flog.info(paste("Found blast databases for the following blast analyses:", paste0(opt$blastanalyses, collapse = ", ")))
+        flog.info(paste("Blast databases are in:", opt$blastpath))
+    } else {
+        opt$blastanalyses <- NULL
+        flog.info("No BLAST databases found in the JAMSdb folder supplied.")
     }
 
-    if(!(is.null(opt$contigs))){
-        opt$host<-"none"
+    #Set host species to none if isolates or if using contigs.
+    if (opt$analysis=="isolate"){
+        opt$host <- "none"
+    }
+
+    if (!(is.null(opt$contigs))){
+        opt$host <- "none"
     }
 
     #Check for host bowtie indices
-    if(opt$host %in% c("mouse", "human")){
+    if (opt$host %in% c("mouse", "human")){
         flog.info("Checking for the presence of pre-compiled host genome bowtie2 indices.")
         #Set host prefix
-        if(opt$host == "mouse"){
-           opt$hostspecies="Mmusculus"
+        if (opt$host == "mouse"){
+           opt$hostspecies <- "Mmusculus"
         } else if (opt$host == "human"){
-           opt$hostspecies="Hsapiens"
+           opt$hostspecies <- "Hsapiens"
         }
-        indexfiles<-list.files(opt$dbdir, recursive=TRUE, full.names=TRUE, include.dirs=FALSE, pattern="*.bt2$")
-        opt$relindexfiles<-indexfiles[grep(opt$hostspecies, indexfiles)]
-        if(length(opt$relindexfiles) == 6){
-            opt$indexpath<-paste0(unlist(strsplit(opt$relindexfiles[1], split="/"))[1:length(unlist(strsplit(opt$relindexfiles[1], split="/")))-1], collapse="/")
+        indexfiles <- list.files(opt$dbdir, recursive=TRUE, full.names=TRUE, include.dirs=FALSE, pattern="*.bt2$")
+        opt$relindexfiles <- indexfiles[grep(opt$hostspecies, indexfiles)]
+        if (length(opt$relindexfiles) == 6){
+            opt$indexpath <- paste0(unlist(strsplit(opt$relindexfiles[1], split="/"))[1:length(unlist(strsplit(opt$relindexfiles[1], split="/")))-1], collapse="/")
             flog.info(paste0("Pre-built bowtie2 indices for the ", opt$host, " genome were found at ", opt$indexpath))
         } else {
             flog.info(paste0("Pre-built bowtie2 indices for the ", opt$host, " genome were not found. Check your database structure. Aborting."))
-            opt$abort<-TRUE
+            opt$abort <- TRUE
         }
     } else if (opt$host == "none") {
         flog.info("No need to eliminate sequencing reads coming from a host species.")
     } else {
         flog.info("Host species supplied is not human, mouse or none.")
-        if(file.exists(opt$host) == TRUE){
+        if (file.exists(opt$host) == TRUE){
             flog.info("Host species bowtie index has been supplied as a path.")
-            opt$relindexfiles<-list.files(opt$host, recursive=TRUE, full.names=TRUE, include.dirs=FALSE, pattern="*.bt2$")
+            opt$relindexfiles <- list.files(opt$host, recursive=TRUE, full.names=TRUE, include.dirs=FALSE, pattern="*.bt2$")
             #Need 6 index files
-            if(length(opt$relindexfiles) == 6){
-                opt$indexpath<-paste0(unlist(strsplit(opt$relindexfiles[1], split="/"))[1:length(unlist(strsplit(opt$relindexfiles[1], split="/")))-1], collapse="/")
-                indexfile1<-unlist(strsplit(opt$relindexfiles[1], split="/"))[length(unlist(strsplit(opt$relindexfiles[1], split="/")))]
-                opt$host<-unlist(strsplit(indexfile1, split="\\."))[1]
-                opt$hostspecies<-opt$host
+            if (length(opt$relindexfiles) == 6){
+                opt$indexpath <- paste0(unlist(strsplit(opt$relindexfiles[1], split="/"))[1:length(unlist(strsplit(opt$relindexfiles[1], split="/")))-1], collapse="/")
+                indexfile1 <- unlist(strsplit(opt$relindexfiles[1], split="/"))[length(unlist(strsplit(opt$relindexfiles[1], split="/")))]
+                opt$host <- unlist(strsplit(indexfile1, split="\\."))[1]
+                opt$hostspecies <- opt$host
                 flog.info(paste0("Pre-built bowtie2 indices for the ", opt$host, " genome were found at ", opt$indexpath))
             } else {
                 flog.info(paste0("Pre-built bowtie2 indices for the host genome were not found or look weird. Check the path supplied. Aborting."))
-                opt$abort<-TRUE
+                opt$abort <- TRUE
             }
         } else {
             flog.info("Host species genome has been supplied as an NCBI species taxid or organism name. Checking Tax ID and best genome to download.")
             flog.info("Trying as Tax ID first.")
-            host_assemblies<-get_genomes_NCBI(organisms = "vertebrate_mammalian", outputdir = NULL, species_taxid = opt$host, nobs = TRUE, fileformat = "fasta", simulate = TRUE)
-            host_assemblies<-subset(host_assemblies, genome_rep == "Full")
+            host_assemblies <- get_genomes_NCBI(organisms = "vertebrate_mammalian", outputdir = NULL, species_taxid = opt$host, nobs = TRUE, fileformat = "fasta", simulate = TRUE)
+            host_assemblies <- subset(host_assemblies, genome_rep == "Full")
             if(nrow(host_assemblies) == 0){
                 flog.info("Not Tax ID. Trying as organism_name.")
-                host_assemblies<-get_genomes_NCBI(organisms = "vertebrate_mammalian", outputdir = NULL, organism_name = opt$host, nobs = TRUE, fileformat = "fasta", simulate = TRUE)
+                host_assemblies <- get_genomes_NCBI(organisms = "vertebrate_mammalian", outputdir = NULL, organism_name = opt$host, nobs = TRUE, fileformat = "fasta", simulate = TRUE)
             }
             #Only consider full genomes
-            host_assemblies<-subset(host_assemblies, genome_rep == "Full")
+            host_assemblies <- subset(host_assemblies, genome_rep == "Full")
             if(nrow(host_assemblies) > 0){
                 flog.info("Deteriming best host genome assembly to download.")
                 #Give priority to reference and then representative genomes, if available.
                 if(any(c("reference genome", "representative genome") %in% host_assemblies$refseq_category)){
-                    refseqcats<-(c("reference genome", "representative genome") %in% host_assemblies$refseq_category)
-                    host_assemblies<-subset(host_assemblies, refseq_category == c("reference genome", "representative genome")[min(which(refseqcats==TRUE))])
+                    refseqcats <- (c("reference genome", "representative genome") %in% host_assemblies$refseq_category)
+                    host_assemblies <- subset(host_assemblies, refseq_category == c("reference genome", "representative genome")[min(which(refseqcats==TRUE))])
                 }
                 #Go with the most recent
-                host_assemblies<-host_assemblies[order(host_assemblies$seq_rel_date, decreasing=TRUE), ][1 , ]
-                opt$host_accession_number<-host_assemblies$assembly_accession
+                host_assemblies <- host_assemblies[order(host_assemblies$seq_rel_date, decreasing=TRUE), ][1 , ]
+                opt$host_accession_number <- host_assemblies$assembly_accession
                 flog.info(paste("Will build index with", host_assemblies$organism_name, "genome under accession number", opt$host_accession_number, "of", host_assemblies$seq_rel_date))
-                opt$host<-paste(host_assemblies$organism_name, opt$host_accession_number, sep="_")
-                opt$hostspecies<-host_assemblies$organism_name
+                opt$host <- paste(host_assemblies$organism_name, opt$host_accession_number, sep="_")
+                opt$hostspecies <- host_assemblies$organism_name
             } else {
                 flog.info("Could not find the Tax ID or organism name to download. Aborting now.")
-                opt$abort<-TRUE
+                opt$abort <- TRUE
             }
         }
     }
 
     #If testing dependencies and leave is true then it is time to abort.
     if(opt$testdependencies == TRUE){
-        opt$abort<-TRUE
+        opt$abort <- TRUE
     }
 
     return(opt)
