48185951c493f177623d3e12fbd29e8729aaa3a7
johnmcculloch <john.anthony.mcculloch@gmail.com>
2019-06-17 12:02:38 -0400

    JAMS_BW version 1.29

--- BWlibexec/install_brew_dependencies.sh
+++ BWlibexec/install_brew_dependencies.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 #Install JAMS dependencies
-module load gcc/8.2.0
+module load gcc/9.1.0
 module load java
 module load python
 module load perl
 cd $HOME
 brew update
 brew tap brewsci/bio
-brew install git wget pigz sratoolkit samtools trimmomatic bowtie2 kraken2 bedtools bedops prokka
-brew install --ignore-dependencies megahit spades
+for JAMSdep in git wget pigz sratoolkit samtools trimmomatic bowtie2 kraken2 bedtools bedops prokka
+do
+    brew ls --versions "$JAMSdep" && brew upgrade "$JAMSdep" || brew install "$JAMSdep"
+done
+
+for JAMSassem in megahit spades
+do
+    brew install --ignore-dependencies "$JAMSassem"
+done
+
 module unload gcc
 module load R
