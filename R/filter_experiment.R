228aae90ae576385399860fdb4238a141d16b0c9
johnmcculloch <john.anthony.mcculloch@gmail.com>
2019-08-08 12:21:34 -0400

    Bug fixes

--- R/filter_experiment.R
+++ R/filter_experiment.R
@@ -5,47 +5,51 @@
 
 filter_experiment <- function(mgseqobj = NULL, featmaxatleastPPM = 0, featcutoff = c(0, 0), samplesToKeep = NULL, featuresToKeep = NULL, asPA = FALSE, asPPM = TRUE, mgSeqnorm = FALSE){
 
+        #If doing PPM get the sum of the matrix BEFOREHAND, so that PPM will be of the complete dataset, and not of the subset.
+        rawcts <- MRcounts(mgseqobj, norm = mgSeqnorm, log = FALSE)
+        totbases <- colSums(rawcts)
+
         #Get only samples you asked for
         if(!(is.null(samplesToKeep))){
             mgseqobj=mgseqobj[ , samplesToKeep]
         }
 
-        #Get only samples you asked for
-        if(!(is.null(featuresToKeep))){
-            mgseqobj=mgseqobj[featuresToKeep, ]
-        }
-
         #If setting featmaxatleastPPM or featcutoff to anything other than the defaults, then return as PPM.
         if((featmaxatleastPPM != 0) || featcutoff != c(0, 0)){
-            asPPM=TRUE
+            asPPM <- TRUE
         }
 
         #Flush out empty rows
-        mgseqobj=mgseqobj[(rowSums(MRcounts(mgseqobj)) > 0), ]
+        mgseqobj <- mgseqobj[(rowSums(MRcounts(mgseqobj)) > 0), ]
         #Flush out empty Samples
-        emptysamples<-names(which(colSums(MRcounts(mgseqobj)) == 0) == TRUE)
+        emptysamples <- names(which(colSums(MRcounts(mgseqobj)) == 0) == TRUE)
         if(length(emptysamples) > 0){
             print(paste("Samples", paste0(emptysamples, collapse=", "), "are empty and will be discarded."))
-            validsamples<-names(which(colSums(MRcounts(mgseqobj)) > 0) == TRUE)
-            mgseqobj=mgseqobj[ , validsamples]
+            validsamples <- names(which(colSums(MRcounts(mgseqobj)) > 0) == TRUE)
+            mgseqobj <- mgseqobj[ , validsamples]
         }
 
         #Transform to PPM if applicable
         if(asPPM == TRUE){
-            mat = MRcounts(mgseqobj, norm = mgSeqnorm, log = FALSE)
-            mat=mat[!is.na(row.names(mat)),]
-            #flush out empty rows
-            #mat=mat[rowSums(mat)>0, ]
+            countmat <- MRcounts(mgseqobj, norm = mgSeqnorm, log = FALSE)
+            countmat <- countmat[!is.na(row.names(countmat)),]
             #transform into PPM
-            mat2<-sapply(1:ncol(mat), function(x){ mat[,x]<-((mat[,x]/(sum(mat[,x])))*1000000)} )
-            colnames(mat2)<-colnames(mat)
-            #Round to integer
-            for(c in 1:ncol(mat2)){
-                mat2[,c]<-round(mat2[,c], 0)
+            getPPM <- function(Sample = NULL){
+                PPMs <- (countmat[ , Sample] / totbases[Sample]) * 1000000
+                return(PPMs)
             }
-            countmatrix<-mat2
-            pheno2<-pData(mgseqobj)
-            ftt<-fData(mgseqobj)
+
+            countmat2 <- sapply(1:ncol(countmat), function(x){ getPPM(colnames(countmat)[x])} )
+            colnames(countmat2) <- colnames(countmat)
+            #Round to integer
+            for(c in 1:ncol(countmat2)){
+                countmat2[, c]<-round(countmat2[, c], 0)
+            }
+            countmatrix <- countmat2
+            pheno2 <- pData(mgseqobj)
+            #just make sure
+            pheno2<-pheno2[colnames(countmatrix), ]
+            ftt <- fData(mgseqobj)
             ##Create a non-normalised class object in metagenomeseq (mgseq)
             phenotypeData = AnnotatedDataFrame(pheno2)
             ttdata = AnnotatedDataFrame(ftt)
@@ -60,16 +64,16 @@ filter_experiment <- function(mgseqobj = NULL, featmaxatleastPPM = 0, featcutoff
             thresholdPPM<-featcutoff[1]
             sampcutoffpct<-min(featcutoff[2], 100)
 
-            featuresToKeep=which((lapply(1:nrow(MRcounts(mgseqobj, norm = FALSE, log = FALSE)), function(x) length(which(MRcounts(mgseqobj, norm = FALSE, log = FALSE)[x,] >= thresholdPPM))/ncol(MRcounts(mgseqobj, norm = FALSE, log = FALSE)))) > (sampcutoffpct/100))
-            mgseqobj=mgseqobj[featuresToKeep, ]
+            featuresToKeep2 <- which((lapply(1:nrow(MRcounts(mgseqobj, norm = FALSE, log = FALSE)), function(x) length(which(MRcounts(mgseqobj, norm = FALSE, log = FALSE)[x,] >= thresholdPPM))/ncol(MRcounts(mgseqobj, norm = FALSE, log = FALSE)))) > (sampcutoffpct/100))
+            mgseqobj=mgseqobj[featuresToKeep2, ]
             cutoffmsg<-paste("Feature must be >", thresholdPPM, "PPM in at least", sampcutoffpct, "% of samples", sep=" ")
             print(cutoffmsg)
         }
 
         if(!(missing(featmaxatleastPPM))){
-            featuresToKeep = which(rowMax(MRcounts(mgseqobj)) >= featmaxatleastPPM)
-            mgseqobj=mgseqobj[featuresToKeep, ]
-            minPPMmsg<-paste("Highest feature must be >", featmaxatleastPPM, "PPM", sep=" ")
+            featuresToKeep2 <- which(rowMax(MRcounts(mgseqobj)) >= featmaxatleastPPM)
+            mgseqobj <- mgseqobj[featuresToKeep2 ]
+            minPPMmsg <- paste("Highest feature must be >", featmaxatleastPPM, "PPM", sep=" ")
             print(minPPMmsg)
         }
 
@@ -77,11 +81,20 @@ filter_experiment <- function(mgseqobj = NULL, featmaxatleastPPM = 0, featcutoff
             countmatrix<-MRcounts(mgseqobj)
             countmatrix[which(countmatrix != 0)]<-1
             pheno2<-pData(mgseqobj)
+            #just make sure
+            pheno2<-pheno2[colnames(countmatrix), ]
             ftt<-fData(mgseqobj)
             ##Create a non-normalised class object in metagenomeseq (mgseq)
             phenotypeData = AnnotatedDataFrame(pheno2)
             ttdata = AnnotatedDataFrame(ftt)
             mgseqobj = newMRexperiment(countmatrix, phenoData=phenotypeData, featureData=ttdata)
+        }
+        #Get only samples you asked for
+
+        if(!(is.null(featuresToKeep))){
+            featurespresent <- rownames(MRcounts(mgseqobj))
+            featuresToKeep <- featuresToKeep[featuresToKeep %in% featurespresent]
+            mgseqobj <- mgseqobj[featuresToKeep, ]
         }
 
         return(mgseqobj)
--- R/make_colour_dictionary.R
+++ R/make_colour_dictionary.R
@@ -5,7 +5,7 @@
 
 make_colour_dictionary <- function(variable_list = NULL, pheno = NULL){
 
-    JAMSpalettenames <- c("royalblue4", "red1", "purple2", "orange2", "chocolate4", "navyblue", "green", "violet", "turquoise2", "tomato", "tan3", "steelblue3", "springgreen2", "violetred1", "slategray3", "wheat3", "violetred4",  "sienna3", "black")
+    JAMSpalettenames <- c("royalblue4", "red1", "purple2", "orange2", "chocolate4", "navyblue", "green", "violet", "turquoise2", "tomato", "tan3", "steelblue3", "springgreen2", "violetred1", "slategray3", "wheat3", "violetred4",  "dodgerblue2", "black")
     JAMSpalette <- rep(JAMSpalettenames, 10)
     #Get list of variables which are discrete
     discretes <- unique(unname(unlist(variable_list[which(!(names(variable_list) %in% c("sample", "continuous")))])))
