81a7cd044e2578c2dccd7780e39d8eeac2a1f990
johnmcculloch <john.anthony.mcculloch@gmail.com>
2019-11-09 01:29:33 -0500

    Improved messaging

--- R/calculate_matrix_stats.R
+++ R/calculate_matrix_stats.R
@@ -43,7 +43,7 @@ calculate_matrix_stats <- function(countmatrix = NULL, uselog = NULL, statsonlog
     }
 
     if ((uselog == TRUE) && (statsonlog == FALSE)){
-        print("Transforming log2 counts back to raw counts for calculating stats.")
+        flog.info("Transforming log2 counts back to raw counts for calculating stats.")
         #log2 transform if applicable
         countmatrix2 <- sapply(1:ncol(countmatrix), function(x){ countmatrix[, x] <- ((2 ^ (countmatrix[, x])) - 1)} )
         colnames(countmatrix2) <- colnames(countmatrix)
@@ -57,7 +57,7 @@ calculate_matrix_stats <- function(countmatrix = NULL, uselog = NULL, statsonlog
     #Calculate matrix stats and get new matrix.
     if(stattype == "variance"){
 
-        print("Calculating variance across samples.")
+        flog.info("Calculating variance across samples.")
         featStatsSD <- apply(countmatrix, 1, sd)
         featStatsMAD <- apply(countmatrix, 1, mad)
         matstats <- data.frame(SD = as.vector(unlist(featStatsSD)), MAD = as.vector(unlist(featStatsMAD)))
@@ -69,7 +69,7 @@ calculate_matrix_stats <- function(countmatrix = NULL, uselog = NULL, statsonlog
 
     } else if (stattype == "binary"){
         if (numclass == 2){
-            print("Calculating p-values with Mann-Whitney-Wilcoxon test.")
+            flog.info("Calculating p-values with Mann-Whitney-Wilcoxon test.")
         } else {
             stop("To calculate p-values with Mann-Whitney-Wilcoxon test, the comparison variable must have exactly two classes.")
         }
@@ -120,11 +120,11 @@ calculate_matrix_stats <- function(countmatrix = NULL, uselog = NULL, statsonlog
         require(parallel)
 
         if (stattype == "permanova"){
-            print("Calculating p-values with permanova. Please be patient...")
-            print(paste("Calculating p-values with PERMANOVA using", nperm, "permutations on", numthreads, "threads. Please be patient..."))
+            flog.info("Calculating p-values with permanova. Please be patient...")
+            flog.info(paste("Calculating p-values with PERMANOVA using", nperm, "permutations on", numthreads, "threads. Please be patient..."))
             mwpval <- sapply(1:nrow(countmatrix), function(x) { vegan::adonis(countmatrix[x,] ~ classesvector, parallel = numthreads, permutations = nperm)$aov.tab$`Pr(>F)`[1] } )
         } else if (stattype == "anova") {
-            print("Calculating p-values with ANOVA. Please be patient...")
+            flog.info("Calculating p-values with ANOVA. Please be patient...")
             mwpval <- sapply(1:nrow(countmatrix), function(x) { summary(aov(countmatrix[x,] ~ classesvector))[[1]]$`Pr(>F)`[1] } )
         }
 
@@ -144,7 +144,7 @@ calculate_matrix_stats <- function(countmatrix = NULL, uselog = NULL, statsonlog
     } else if (stattype == "PA"){
         require(parallel)
         if (numclass == 2){
-            print("Calculating p-values for presence/absence testing with Fisher s exact test.")
+            flog.info("Calculating p-values for presence/absence testing with Fisher s exact test.")
         } else {
             stop("To calculate p-values for presence/absence testing, the comparison variable must have exactly two classes.")
         }
@@ -170,7 +170,7 @@ calculate_matrix_stats <- function(countmatrix = NULL, uselog = NULL, statsonlog
         corstat <- stattype
         #Either correlate to variable or do pairwise feature comparison
         if (is.null(classesvector)){
-            print("Calculating pairwise correlation coefficients of all features")
+            flog.info("Calculating pairwise correlation coefficients of all features")
             tcountmatrix <- t(countmatrix)
             matstats <- stats::cor(tcountmatrix, method = corstat)
         } else {
