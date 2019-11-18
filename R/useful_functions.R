5bf4f02e7e07ab40313818dedd28954b59373041
johnmcculloch <john.anthony.mcculloch@gmail.com>
2019-11-17 22:15:35 -0500

    Removed functions defined within other functions

--- R/useful_functions.R
+++ R/useful_functions.R
@@ -222,3 +222,134 @@ can_be_made_numeric <- function(x, cats_to_ignore = NULL){
 
     return(testresult)
 }
+
+
+#' filter_correlations(corrmat = NULL, mincorrelcoeff = NULL)
+#' Given a pairwise correlation matrix, eliminate features which do not present an absolute correlation coefficient smaller than mincorrelcoeff with all other features other than itself.
+#'
+#' @export
+ filter_correlations <- function(corrmat = NULL, mincorrelcoeff = NULL){
+
+     if(nrow(corrmat) != ncol(corrmat)){
+         stop("Correlation matrix must have equal numbers of rows and columns.")
+     }
+
+     featsIwant <- NULL
+
+     for (rw in 1:nrow(corrmat)){
+         featint <- rownames(corrmat)[rw]
+         #print(paste("Checking:", featint))
+         correlations <- corrmat[which(rownames(corrmat) != featint), featint]
+
+         if(max(abs(correlations)) >= mincorrelcoeff){
+             feat <- featint
+         } else {
+             feat <- NULL
+         }
+
+         featsIwant <- append(featsIwant, feat)
+
+     }
+
+     corrmat <- corrmat[featsIwant, featsIwant]
+
+     return(corrmat)
+ }
+
+
+ #' getN50(contigs2length)
+ #' JAMSalpha function to get N50 from contigs2length
+ #'
+ #' @export
+ getN50 <- function(contigs2length){
+     quantile50 <- (sum(as.numeric(contigs2length$Length))) * 0.5
+     N50 <- max(subset(contigs2length, Lcum > quantile50)$Length)
+     return(N50)
+ }
+
+ #' getL50(contigs2length)
+ #' JAMSalpha function to get L50 from contigs2length
+ #'
+ #' @export
+ getL50 <- function(contigs2length){
+     quantile50 <- (sum(as.numeric(contigs2length$Length))) * 0.5
+     L50 <- length(subset(contigs2length, Lcum > quantile50)$Length)
+     return(L50)
+ }
+
+ #' getN90(contigs2length)
+ #' JAMSalpha function to get N90 from contigs2length
+ #'
+ #' @export
+ getN90 <- function(contigs2length){
+     quantile90 <- (sum(as.numeric(contigs2length$Length))) * 0.9
+     N90 <- max(subset(contigs2length, Lcum > quantile90)$Length)
+     return(N90)
+ }
+
+
+ #' estimate_genome_completeness(contigs2length)
+ #' JAMSalpha function to estimate genome completeness from contigs2length
+ #'
+ #' @export
+ estimate_genome_completeness <- function(contigs2length = NULL){
+     taxinteresttype <- gsub("^LKT__", "", unique(contigs2length$Taxon))
+     expgensize <- as.integer(JAMSMedian_Genome_Sizes[taxinteresttype, "Genome_Size_Median"])
+     if ((length(expgensize) < 1) | (is.na(expgensize)) | (taxinteresttype %in% c("Unclassified", "Missing", "Unclassified__Unclassified"))){
+         expgensize <- as.integer(JAMSMedian_Genome_Sizes["k__Bacteria", "Genome_Size_Median"])
+     }
+     probnumgen <- round((sum(contigs2length$Length) / expgensize), 2)
+     return(probnumgen)
+ }
+
+ #' find16SrRNA(contigs2length = NULL, opt = NULL)
+ #' JAMSalpha function to find number of 16S genes from contigs2length
+ #'
+ #' @export
+ find16SrRNA <- function(contigs2length = NULL, opt = NULL){
+     featuredata = opt$featuredata
+     #Find how many 16S rRNAs which have been classified can be found in the contigs were looking at.
+     num16S <- length(which((subset(featuredata, Contig %in% contigs2length$Contig)[]$SixteenSid) != "none"))
+
+     return(num16S)
+ }
+
+
+ #' getcontigs2length(opt = NULL, taxlevel = NULL, taxinterest = NULL)
+ #' JAMSalpha function to compute total contig length of a taxon from contigs2length
+ #'
+ #' @export
+ getcontigs2length <- function(opt = NULL, taxlevel = NULL, taxinterest = NULL){
+     contigsdata <- opt$contigsdata
+     contigs2length <- subset(contigsdata, contigsdata[,taxlevel] == taxinterest)
+     contigs2length <- contigs2length[, c(taxlevel, "Contig", "Length")]
+     colnames(contigs2length) <- c("Taxon", "Contig", "Length")
+     contigs2length <- contigs2length[order(contigs2length$Length, decreasing = TRUE), ]
+     contigs2length$Lcum <- cumsum(contigs2length$Length)
+     return(contigs2length)
+ }
+
+
+ #' get_assembly_stats_by_taxon(opt = NULL, taxlevel = NULL, taxinterest = NULL)
+ #' JAMSalpha function to compute assembly statistics by taxon
+ #'
+ #' @export
+ get_assembly_stats_by_taxon <- function(opt = NULL, taxlevel = NULL, taxinterest = NULL){
+     contigs2length <- getcontigs2length(opt=opt, taxlevel=taxlevel, taxinterest = taxinterest)
+     assemblystats <- data.frame(TaxLevel = taxlevel, Taxon = taxinterest, NumContigs = nrow(contigs2length), ContigSum = sum(contigs2length$Length), LargestContigSize = max(contigs2length$Length), N50 = getN50(contigs2length), L50 = getL50(contigs2length), N90 = getN90(contigs2length), ProbNumGenomes = estimate_genome_completeness(contigs2length), Num16S = find16SrRNA(contigs2length = contigs2length, opt = opt))
+     return(assemblystats)
+ }
+
+ #' get_assembly_stats_by_taxlevel(opt = NULL, taxlevel = NULL)
+ #' JAMSalpha function to compute assembly statistics by taxonomic level
+ #'
+ #' @export
+ get_assembly_stats_by_taxlevel <- function(opt = NULL, taxlevel = NULL){
+     contigsdata <- opt$contigsdata
+     #Get a list of non-redundant tax entities
+     Taxa <- unique(contigsdata[ , taxlevel])
+     assemblystats_taxlevel <- lapply(Taxa, function(t) { get_assembly_stats_by_taxon(opt = opt, taxlevel = taxlevel, taxinterest = t) })
+     assemblystats_taxlevel <- plyr::ldply(assemblystats_taxlevel, rbind)
+     assemblystats_taxlevel <- assemblystats_taxlevel[order(assemblystats_taxlevel$ProbNumGenomes, decreasing = TRUE), ]
+     return(assemblystats_taxlevel)
+ }
