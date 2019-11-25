83aa62006feb447f5700a28db73b1bb8c84b5846
johnmcculloch <john.anthony.mcculloch@gmail.com>
2019-11-25 12:49:39 -0500

    Fixed declaring functions that needed to be delclared within functions

--- R/useful_functions.R
+++ R/useful_functions.R
@@ -256,105 +256,6 @@ can_be_made_numeric <- function(x, cats_to_ignore = NULL){
      return(corrmat)
  }
 
-
- #' getN50(contigs2length)
- #' JAMSalpha function to get N50 from contigs2length
- #'
- #' @export
- getN50 <- function(contigs2length){
-     quantile50 <- (sum(as.numeric(contigs2length$Length))) * 0.5
-     N50 <- max(subset(contigs2length, Lcum > quantile50)$Length)
-     return(N50)
- }
-
- #' getL50(contigs2length)
- #' JAMSalpha function to get L50 from contigs2length
- #'
- #' @export
- getL50 <- function(contigs2length){
-     quantile50 <- (sum(as.numeric(contigs2length$Length))) * 0.5
-     L50 <- length(subset(contigs2length, Lcum > quantile50)$Length)
-     return(L50)
- }
-
- #' getN90(contigs2length)
- #' JAMSalpha function to get N90 from contigs2length
- #'
- #' @export
- getN90 <- function(contigs2length){
-     quantile90 <- (sum(as.numeric(contigs2length$Length))) * 0.9
-     N90 <- max(subset(contigs2length, Lcum > quantile90)$Length)
-     return(N90)
- }
-
-
- #' estimate_genome_completeness(contigs2length)
- #' JAMSalpha function to estimate genome completeness from contigs2length
- #'
- #' @export
- estimate_genome_completeness <- function(contigs2length = NULL){
-     taxinteresttype <- gsub("^LKT__", "", unique(contigs2length$Taxon))
-     expgensize <- as.integer(JAMSMedian_Genome_Sizes[taxinteresttype, "Genome_Size_Median"])
-     if ((length(expgensize) < 1) | (is.na(expgensize)) | (taxinteresttype %in% c("Unclassified", "Missing", "Unclassified__Unclassified"))){
-         expgensize <- as.integer(JAMSMedian_Genome_Sizes["k__Bacteria", "Genome_Size_Median"])
-     }
-     probnumgen <- round((sum(contigs2length$Length) / expgensize), 2)
-     return(probnumgen)
- }
-
- #' find16SrRNA(contigs2length = NULL, opt = NULL)
- #' JAMSalpha function to find number of 16S genes from contigs2length
- #'
- #' @export
- find16SrRNA <- function(contigs2length = NULL, opt = NULL){
-     featuredata = opt$featuredata
-     #Find how many 16S rRNAs which have been classified can be found in the contigs were looking at.
-     num16S <- length(which((subset(featuredata, Contig %in% contigs2length$Contig)[]$SixteenSid) != "none"))
-
-     return(num16S)
- }
-
-
- #' getcontigs2length(opt = NULL, taxlevel = NULL, taxinterest = NULL)
- #' JAMSalpha function to compute total contig length of a taxon from contigs2length
- #'
- #' @export
- getcontigs2length <- function(opt = NULL, taxlevel = NULL, taxinterest = NULL){
-     contigsdata <- opt$contigsdata
-     contigs2length <- subset(contigsdata, contigsdata[,taxlevel] == taxinterest)
-     contigs2length <- contigs2length[, c(taxlevel, "Contig", "Length")]
-     colnames(contigs2length) <- c("Taxon", "Contig", "Length")
-     contigs2length <- contigs2length[order(contigs2length$Length, decreasing = TRUE), ]
-     contigs2length$Lcum <- cumsum(contigs2length$Length)
-     return(contigs2length)
- }
-
-
- #' get_assembly_stats_by_taxon(opt = NULL, taxlevel = NULL, taxinterest = NULL)
- #' JAMSalpha function to compute assembly statistics by taxon
- #'
- #' @export
- get_assembly_stats_by_taxon <- function(opt = NULL, taxlevel = NULL, taxinterest = NULL){
-     contigs2length <- getcontigs2length(opt=opt, taxlevel=taxlevel, taxinterest = taxinterest)
-     assemblystats <- data.frame(TaxLevel = taxlevel, Taxon = taxinterest, NumContigs = nrow(contigs2length), ContigSum = sum(contigs2length$Length), LargestContigSize = max(contigs2length$Length), N50 = getN50(contigs2length), L50 = getL50(contigs2length), N90 = getN90(contigs2length), ProbNumGenomes = estimate_genome_completeness(contigs2length), Num16S = find16SrRNA(contigs2length = contigs2length, opt = opt))
-     return(assemblystats)
- }
-
- #' get_assembly_stats_by_taxlevel(opt = NULL, taxlevel = NULL)
- #' JAMSalpha function to compute assembly statistics by taxonomic level
- #'
- #' @export
- get_assembly_stats_by_taxlevel <- function(opt = NULL, taxlevel = NULL){
-     contigsdata <- opt$contigsdata
-     #Get a list of non-redundant tax entities
-     Taxa <- unique(contigsdata[ , taxlevel])
-     assemblystats_taxlevel <- lapply(Taxa, function(t) { get_assembly_stats_by_taxon(opt = opt, taxlevel = taxlevel, taxinterest = t) })
-     assemblystats_taxlevel <- plyr::ldply(assemblystats_taxlevel, rbind)
-     assemblystats_taxlevel <- assemblystats_taxlevel[order(assemblystats_taxlevel$ProbNumGenomes, decreasing = TRUE), ]
-     return(assemblystats_taxlevel)
- }
-
-
  #' IO_jams_workspace_image(opt = NULL, workspaceimage = NULL, threads = 8, operation = c("save", "load"))
  #' Safe way of either loading or saving an R workspace image. If argument workspaceimage is null, workspace image file will be searched for in opt (opt$projimage). If that is also NULL, saving or loading is aborted. If the fastSave package () is installed, multi-threaded loading or saving will be used. If opt is passed, number of CPUs will be set to opt$threads, trumping the threads argument.
  #'
@@ -435,4 +336,14 @@ spew_heatmap_report <- function(hmcomb){
     }
 
     setwd(opt$outdir)
+}
+
+
+#' maxRAMbytes_used()
+#' Wrapper for launching a report for a SINGLE analysis
+#'
+#' @export
+maxRAMbytes_used <- function(){
+    RAMbytes <- sum(gc(full = TRUE)[,6]) * 1024
+    return(RAMbytes)
 }
