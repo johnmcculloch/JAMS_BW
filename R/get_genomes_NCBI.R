3f54074337ad2514b3a55452abc04de1040d8bc2
johnmcculloch <john.anthony.mcculloch@gmail.com>
2019-11-25 13:43:47 -0500

    Using flog.info instead of print

--- R/get_genomes_NCBI.R
+++ R/get_genomes_NCBI.R
@@ -15,7 +15,7 @@ get_genomes_NCBI <- function(organisms = "bacteria", assembly_summary = NULL, ou
     }
 
     if (is.null(assembly_summary)){
-        print("Downloading assembly_summary.txt from GenBank")
+        flog.info("Downloading assembly_summary.txt from GenBank")
         GBURL <- "https://ftp.ncbi.nlm.nih.gov/genomes/genbank"
         if (!is.null(organisms)){
             ASURL <- paste(GBURL, organisms, "assembly_summary.txt", sep = "/")
@@ -29,46 +29,46 @@ get_genomes_NCBI <- function(organisms = "bacteria", assembly_summary = NULL, ou
         #Fix column names
         colnames(assembly_summary) <- gsub(" ", "", gsub("#", "", colnames(assembly_summary)))
         #Correct table to make consistent with JAMS taxtable
-        assembly_summary$taxid<-as.character(assembly_summary$taxid)
-        assembly_summary$species_taxid<-as.character(assembly_summary$species_taxid)
-        assembly_summary$infraspecific_name<-gsub("strain=", "", assembly_summary$infraspecific_name)
-        assembly_summary$asm_name<-gsub(" ", "_", assembly_summary$asm_name)
+        assembly_summary$taxid <- as.character(assembly_summary$taxid)
+        assembly_summary$species_taxid <- as.character(assembly_summary$species_taxid)
+        assembly_summary$infraspecific_name <- gsub("strain=", "", assembly_summary$infraspecific_name)
+        assembly_summary$asm_name <- gsub(" ", "_", assembly_summary$asm_name)
         fix_names<-function(asdf, colm){
-            asdf[, colm]<-gsub("[^[:alnum:] ]", "_", asdf[, colm])
-            asdf[, colm]<-gsub("^root", "Unclassified", asdf[, colm])
-            asdf[, colm]<-gsub("unclassified", "Unclassified", asdf[, colm])
-            asdf[, colm]<-gsub(" ", "_", asdf[, colm])
-            asdf[, colm]<-gsub("__", "_", asdf[, colm])
-            asdf[, colm]<-gsub("___", "_", asdf[, colm])
-            asdf[, colm]<-gsub("^_", "", asdf[, colm])
-            asdf[, colm]<-gsub("_sp_", "_Unclassified_", asdf[, colm])
-            asdf[, colm]<-gsub("_$", "", asdf[, colm])
+            asdf[, colm] <- gsub("[^[:alnum:] ]", "_", asdf[, colm])
+            asdf[, colm] <- gsub("^root", "Unclassified", asdf[, colm])
+            asdf[, colm] <- gsub("unclassified", "Unclassified", asdf[, colm])
+            asdf[, colm] <- gsub(" ", "_", asdf[, colm])
+            asdf[, colm] <- gsub("__", "_", asdf[, colm])
+            asdf[, colm] <- gsub("___", "_", asdf[, colm])
+            asdf[, colm] <- gsub("^_", "", asdf[, colm])
+            asdf[, colm] <- gsub("_sp_", "_Unclassified_", asdf[, colm])
+            asdf[, colm] <- gsub("_$", "", asdf[, colm])
 
             return(asdf)
         }
-        assembly_summary<-fix_names(asdf=assembly_summary, colm="organism_name")
-        assembly_summary<-fix_names(asdf=assembly_summary, colm="infraspecific_name")
+        assembly_summary <- fix_names(asdf=assembly_summary, colm="organism_name")
+        assembly_summary <- fix_names(asdf=assembly_summary, colm="infraspecific_name")
     }
 
-    print(paste("There are", nrow(assembly_summary), "entries for", organisms, "genomes in NCBI GenBank."))
+    flog.info(paste("There are", nrow(assembly_summary), "entries for", organisms, "genomes in NCBI GenBank."))
     wanted_assembly_summary <- assembly_summary
 
     if (!is.null(assembly_accession)){
-        print(paste("Subsetting to entries only with assembly accession(s)", paste0(assembly_accession, collapse=", ")))
+        flog.info(paste("Subsetting to entries only with assembly accession(s)", paste0(assembly_accession, collapse=", ")))
         wanted_assembly_accession <- assembly_accession
         wanted_assembly_summary <- subset(wanted_assembly_summary, (assembly_accession %in% wanted_assembly_accession))
-        print(paste("There are", nrow(wanted_assembly_summary), "entries matching the assembly accession criteria."))
+        flog.info(paste("There are", nrow(wanted_assembly_summary), "entries matching the assembly accession criteria."))
     }
 
-    if(!is.null(bioproject)){
-        print(paste("Subsetting to entries only within BioProject(s)", paste0(bioproject, collapse=", ")))
+    if (!is.null(bioproject)){
+        flog.info(paste("Subsetting to entries only within BioProject(s)", paste0(bioproject, collapse=", ")))
         wanted_bioproject<-bioproject
         wanted_assembly_summary<-subset(wanted_assembly_summary, (bioproject %in% wanted_bioproject))
         print(paste("There are", nrow(wanted_assembly_summary), "entries matching the BioProject criteria."))
     }
 
     if(!is.null(biosample)){
-        print(paste("Subsetting to entries only within BioSample(s)", paste0(biosample, collapse=", ")))
+        flog.info(paste("Subsetting to entries only within BioSample(s)", paste0(biosample, collapse=", ")))
         wanted_biosample<-biosample
         wanted_assembly_summary<-subset(wanted_assembly_summary, (biosample %in% wanted_biosample))
         print(paste("There are", nrow(wanted_assembly_summary), "entries matching the BioSample criteria."))
@@ -79,17 +79,17 @@ get_genomes_NCBI <- function(organisms = "bacteria", assembly_summary = NULL, ou
 
         if(is.null(species_taxid)){
             #Get wanted species taxid from organism_name
-            print("Subsetting by Organism Name")
+            flog.info("Subsetting by Organism Name")
             if(as_general_expression != TRUE){
-                 wanted_species_taxid<-unique(wanted_assembly_summary$species_taxid[which(wanted_assembly_summary[ , "organism_name"] %in% organism_name)])
+                 wanted_species_taxid <- unique(wanted_assembly_summary$species_taxid[which(wanted_assembly_summary[ , "organism_name"] %in% organism_name)])
             } else {
-                print("Considering organism_name supplied to be a general expression. Will select all Organism Names matching the expression(s) supplied.")
-                wantedrowslist<-unlist(lapply(1:length(organism_name), function (x) { grep(organism_name[x], wanted_assembly_summary[ , "organism_name"]) }))
-                wanted_species_taxid<-unique(wanted_assembly_summary$species_taxid[wantedrowslist])
+                flog.info("Considering organism_name supplied to be a general expression. Will select all Organism Names matching the expression(s) supplied.")
+                wantedrowslist <- unlist(lapply(1:length(organism_name), function (x) { grep(organism_name[x], wanted_assembly_summary[ , "organism_name"]) }))
+                wanted_species_taxid <- unique(wanted_assembly_summary$species_taxid[wantedrowslist])
             }
         } else {
-            print("Subsetting by Species Tax ID (species_taxid)")
-            wanted_species_taxid<-species_taxid
+            flog.info("Subsetting by Species Tax ID (species_taxid)")
+            wanted_species_taxid <- species_taxid
         }
 
         if(!is.null(taxid)){
@@ -97,82 +97,82 @@ get_genomes_NCBI <- function(organisms = "bacteria", assembly_summary = NULL, ou
             wanted_species_taxid<-unique(append(wanted_species_taxid, unique(wanted_assembly_summary$species_taxid[which(wanted_assembly_summary[ , "taxid"] %in% taxid)])))
         }
 
-        wanted_assembly_summary<-subset(wanted_assembly_summary, (species_taxid %in% wanted_species_taxid))
-        print(paste("There are", nrow(wanted_assembly_summary), "entries matching the Species criteria."))
+        wanted_assembly_summary <- subset(wanted_assembly_summary, (species_taxid %in% wanted_species_taxid))
+        flog.info(paste("There are", nrow(wanted_assembly_summary), "entries matching the Species criteria."))
     }
 
     #Subset by infraspecies criteria
     if(!is.null(taxid)){
-        print("Filtering by Tax ID")
+        flog.info("Filtering by Tax ID")
         wantedtaxid<-taxid
         wanted_assembly_summary<-subset(wanted_assembly_summary, (taxid %in% wantedtaxid))
         print(paste("There are", nrow(wanted_assembly_summary), "entries matching the Tax ID criteria."))
     } else {
-        if(!is.null(infraspecific_name)){
-            print("Filtering by strain name")
+        if (!is.null(infraspecific_name)){
+            flog.info("Filtering by strain name")
             strainnames<-infraspecific_name
             wanted_assembly_summary<-subset(wanted_assembly_summary, (infraspecific_name %in% strainnames))
-            print(paste("There are", nrow(wanted_assembly_summary), "entries matching the Strain Name criteria."))
+            flog.info(paste("There are", nrow(wanted_assembly_summary), "entries matching the Strain Name criteria."))
         }
 
-        if(!is.null(asm_name)){
-            print("Filtering by asm_name")
-            asmnames<-asm_name
+        if (!is.null(asm_name)){
+            flog.info("Filtering by asm_name")
+            asmnames <- asm_name
             if(as_general_expression != TRUE){
-                wanted_assembly_summary<-subset(wanted_assembly_summary, (asm_name %in% asmnames))
+                wanted_assembly_summary <- subset(wanted_assembly_summary, (asm_name %in% asmnames))
             } else {
-                print("Considering asm_name supplied to be a general expression. Will select all asm names matching the expression(s) supplied.")
-                wantedrowslist<-unlist(lapply(1:length(asmnames), function (x) { grep(asmnames[x], wanted_assembly_summary[ , "asm_name"]) }))
-                wanted_assembly_summary<-wanted_assembly_summary[unique(wantedrowslist) , ]
+                flog.info("Considering asm_name supplied to be a general expression. Will select all asm names matching the expression(s) supplied.")
+                wantedrowslist <- unlist(lapply(1:length(asmnames), function (x) { grep(asmnames[x], wanted_assembly_summary[ , "asm_name"]) }))
+                wanted_assembly_summary <- wanted_assembly_summary[unique(wantedrowslist) , ]
             }
-            print(paste("There are", nrow(wanted_assembly_summary), "entries matching the asm name criteria."))
+            flog.info(paste("There are", nrow(wanted_assembly_summary), "entries matching the asm name criteria."))
         }
     }
 
     ##Subset by quality criteria
     #By assembly level
-    if(is.null(assembly_level)){
+    if (is.null(assembly_level)){
         if(!is.null(assembly_upto)){
-            wanted_assembly_level<-switch(assembly_upto, "Contig"=c("Contig", "Scaffold", "Chromosome", "Complete Genome"), "Scaffold"=c("Scaffold", "Chromosome", "Complete Genome"), "Chromosome"=c("Chromosome", "Complete genome"), "Complete"="Complete Genome")
+            wanted_assembly_level <- switch(assembly_upto, "Contig"=c("Contig", "Scaffold", "Chromosome", "Complete Genome"), "Scaffold"=c("Scaffold", "Chromosome", "Complete Genome"), "Chromosome"=c("Chromosome", "Complete genome"), "Complete"="Complete Genome")
         } else {
-            wanted_assembly_level<-c("Contig", "Scaffold", "Chromosome", "Complete Genome")
+            wanted_assembly_level <- c("Contig", "Scaffold", "Chromosome", "Complete Genome")
         }
     }
-    wanted_assembly_summary<-subset(wanted_assembly_summary, (assembly_level %in% wanted_assembly_level))
-    print(paste("There are", nrow(wanted_assembly_summary), "entries matching the Assembly Level criteria."))
+    wanted_assembly_summary <- subset(wanted_assembly_summary, (assembly_level %in% wanted_assembly_level))
+    flog.info(paste("There are", nrow(wanted_assembly_summary), "entries matching the Assembly Level criteria."))
 
-    if(!is.null(refseq_category_upto)){
-        wanted_refseq_category<-switch(refseq_category_upto, "reference" = "reference genome", "representative" = c("reference genome", "representative genome"), "all" = c("reference genome", "representative genome", "na"))
-        print(paste("Subsetting by RefSeq category to include only those within", paste0(wanted_refseq_category, collapse=", "), "categories."))
-        wanted_assembly_summary<-subset(wanted_assembly_summary, (refseq_category %in% wanted_refseq_category))
-        print(paste("There are", nrow(wanted_assembly_summary), "entries matching the RefSeq criteria."))
+    if (!is.null(refseq_category_upto)){
+        wanted_refseq_category <- switch(refseq_category_upto, "reference" = "reference genome", "representative" = c("reference genome", "representative genome"), "all" = c("reference genome", "representative genome", "na"))
+        flog.info(paste("Subsetting by RefSeq category to include only those within", paste0(wanted_refseq_category, collapse=", "), "categories."))
+        wanted_assembly_summary <- subset(wanted_assembly_summary, (refseq_category %in% wanted_refseq_category))
+        flog.info(paste("There are", nrow(wanted_assembly_summary), "entries matching the RefSeq criteria."))
     }
 
     if(nobs == TRUE){
-        print("Filtering out entries which have been tagged as being problematic or low quality.")
-        noproblem<-which(wanted_assembly_summary$excluded_from_refseq == "")
-        wanted_assembly_summary<-wanted_assembly_summary[noproblem, ]
-        print(paste("There are", nrow(wanted_assembly_summary), "entries which have NOT been flagged as being problematic or low quality."))
+        flog.info("Filtering out entries which have been tagged as being problematic or low quality.")
+        noproblem <- which(wanted_assembly_summary$excluded_from_refseq == "")
+        wanted_assembly_summary <- wanted_assembly_summary[noproblem, ]
+        flog.info(paste("There are", nrow(wanted_assembly_summary), "entries which have NOT been flagged as being problematic or low quality."))
     }
 
-    if(nrow(wanted_assembly_summary) > 0){
+    if (nrow(wanted_assembly_summary) > 0){
         #Fix URLs to match filetype wanted
-        filesuffix<-switch(fileformat, "genbank" = "genomic.gbff.gz", "fasta" = "genomic.fna.gz", "gff" = "genomic.gff.gz", "faa" = "protein.faa.gz", "rna" = "rna_from_genomic.fna.gz")
+        filesuffix <- switch(fileformat, "genbank" = "genomic.gbff.gz", "fasta" = "genomic.fna.gz", "gff" = "genomic.gff.gz", "faa" = "protein.faa.gz", "rna" = "rna_from_genomic.fna.gz")
         #Get the repeat bit needed for each ftp_path. Sigh.
-        sufreps<-sapply(1:nrow(wanted_assembly_summary), function (x) {unlist(strsplit(wanted_assembly_summary$ftp_path[x], split="/"))[length(unlist(strsplit(wanted_assembly_summary$ftp_path[x], split="/")))]})
-        wanted_assembly_summary$url<-paste(wanted_assembly_summary$ftp_path, paste(sufreps, filesuffix, sep="_")  , sep="/")
-        wanted_assembly_summary$destfn<-file.path(outputdir, paste(wanted_assembly_summary$organism_name, wanted_assembly_summary$infraspecific_name, wanted_assembly_summary$assembly_accession, filesuffix, sep="_"))
-        wanted_assembly_summary$ftp_path<-NULL
+        sufreps <- sapply(1:nrow(wanted_assembly_summary), function (x) {unlist(strsplit(wanted_assembly_summary$ftp_path[x], split="/"))[length(unlist(strsplit(wanted_assembly_summary$ftp_path[x], split="/")))]})
+        wanted_assembly_summary$url <- paste(wanted_assembly_summary$ftp_path, paste(sufreps, filesuffix, sep="_")  , sep="/")
+        wanted_assembly_summary$destfn <- file.path(outputdir, paste(wanted_assembly_summary$organism_name, wanted_assembly_summary$infraspecific_name, wanted_assembly_summary$assembly_accession, filesuffix, sep="_"))
+        wanted_assembly_summary$ftp_path <- NULL
 
         #Now download stuff wanted to the output dir
         if(simulate == FALSE){
-            print("Downloading genomes")
+            flog.info("Downloading genomes")
             dnl <- sapply(1:nrow(wanted_assembly_summary), function (x) { tryCatch(download.file(wanted_assembly_summary$url[x], destfile = wanted_assembly_summary$destfn[x] ), error = function(e) print(paste("Unable to download", wanted_assembly_summary$url[x]))) } )
         }
 
     } else {
-        print("No genomes fit the criteria imposed.")
-        wanted_assembly_summary<-NULL
+        flog.info("No genomes fit the criteria imposed.")
+        wanted_assembly_summary <- NULL
     }
 
     return(wanted_assembly_summary)
