6bdd216e03a27eff2b479f23645fd6d945127a36
johnmcculloch <john.anthony.mcculloch@gmail.com>
2020-04-16 01:11:56 -0400

    Fixed typo

--- R/useful_functions.R
+++ R/useful_functions.R
@@ -348,7 +348,7 @@ spew_heatmap_report <- function(hmcomb){
 
     }  else if (report == "alpha") {
 
-        tryCatch(project = project, expvec = expvec, usefulexp = analysis, variable_list = variable_list, measures = c("Observed", "InvSimpson", "GeneCounts"), cdict = cdict, stratify_by_kingdoms = TRUE, applyfilters = NULL, PPM_normalize_to_bases_sequenced = FALSE, max_pairwise_cats = 4, ignoreunclassified = TRUE, class_to_ignore = opt$class_to_ignore), error = function(e) whoopsieplot(paste("generating alpha diversity plots for", analysis, "analysis")))
+        tryCatch(make_alpha_report(project = project, expvec = expvec, usefulexp = analysis, variable_list = variable_list, measures = c("Observed", "InvSimpson", "GeneCounts"), cdict = cdict, stratify_by_kingdoms = TRUE, applyfilters = NULL,  GenomeCompletenessCutoff = c(5, 5), PPM_normalize_to_bases_sequenced = FALSE, max_pairwise_cats = 4, ignoreunclassified = TRUE, class_to_ignore = opt$class_to_ignore), error = function(e) whoopsieplot(paste("generating alpha diversity plots for", analysis, "analysis")))
     }
 
     setwd(opt$outdir)
