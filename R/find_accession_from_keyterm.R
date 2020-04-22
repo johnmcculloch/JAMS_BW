4b86a135f9b8ed4db9e19f82da233fab640414ba
johnmcculloch <john.anthony.mcculloch@gmail.com>
2020-04-22 05:33:47 -0400

    Multiple keyterms accepted now

--- R/find_accession_from_keyterm.R
+++ R/find_accession_from_keyterm.R
@@ -3,20 +3,19 @@
 #' Returns the accession number(s) of a feature(s) matching a general expression present in a SummarizedExperiment or metagenomeSeq object.
 #' @export
 
-find_accession_from_keyterm<-function(ExpObj = NULL, keyterm = NULL){
+find_accession_from_keyterm <- function(ExpObj = NULL, keyterm = NULL){
 
-    if (as.character(class(ExpObj)) == "SummarizedExperiment"){
-        feattable <- rowData(ExpObj)
-    } else {
-        feattable <- fData(ExpObj)
+    feattable <- rowData(ExpObj)
+    accessionvector <- NULL
+    feattable$Feature <- paste(feattable$Accession, feattable$Description, sep="-")
+
+    #Get info by general expression matching
+    for (kt in keyterm){
+        rowsIwant <- grep(kt, feattable$Description, ignore.case = TRUE)
+        features <- feattable$Feature[rowsIwant]
+        print(features)
+        accessionvector <- c(accessionvector, as.vector(feattable$Accession[rowsIwant]))
     }
 
-    feattable$Feature <- paste(feattable$Accession, feattable$Description, sep="-")
-    #Get info by general expression matching
-    rowsIwant <- grep(keyterm, feattable$Description, ignore.case = TRUE)
-    feature <- feattable$Feature[rowsIwant]
-    print(feature)
-    accession <- as.vector(feattable$Accession[rowsIwant])
-
-    return(accession)
+    return(accessionvector)
 }
