687b509f21bdc33a3daa10cffb668f52c029e941
johnmcculloch <john.anthony.mcculloch@gmail.com>
2020-05-14 01:12:26 -0400

    Cleaned up code

--- R/installbioconductordependencies.R
+++
@@ -1,6 +0,0 @@
-install_biocon_deps <- function(){
-bioconductordeps <- c("dada2", "metagenomeSeq", "ComplexHeatmap", "HybridMTest", "genefilter")
-    if (!requireNamespace("BiocManager", quietly = TRUE))
-        install.packages("BiocManager")
-    BiocManager::install(bioconductordeps)
-}
