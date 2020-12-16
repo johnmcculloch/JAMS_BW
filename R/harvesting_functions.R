635da969f43947310bb16acb8775189a81815ff9
johnmcculloch <john.anthony.mcculloch@gmail.com>
2020-12-15 23:36:01 -0500

    In the interest of stability, reverted to non-parallel addition of interpro results to featuredata

--- R/harvesting_functions.R
+++ R/harvesting_functions.R
@@ -3,6 +3,50 @@
 #' JAMSalpha function
 #' @export
 
+add_interpro_to_featuredata <- function(opt = NULL, doinparallel = FALSE){
+
+    #Define which interpro analyses I have
+    iproanalyses <- sort(unique(opt$interproscanoutput$Analysis))
+    if ("IproAcc" %in% colnames(opt$interproscanoutput)){
+        iproanalyses <- c(iproanalyses, "Interpro")
+    }
+    if ("GOterms" %in% colnames(opt$interproscanoutput)){
+        iproanalyses <- c(iproanalyses, "GO")
+    }
+    if ("Pathways" %in% colnames(opt$interproscanoutput)){
+        iproanalyses <- c(iproanalyses, "MetaCyc")
+    }
+    flog.info(paste("Found the following Interproscan analyses to harvest:", paste0(iproanalyses, collapse = ", ")))
+
+    if (doinparallel == TRUE){
+
+        appropriatenumcores <- max(1 , (min((opt$threads - 2), length(iproanalyses))))
+        flog.info(paste("Adding Interproscan analyses signatures to featuredata with", appropriatenumcores, "CPUs. Please be patient."))
+        iproanalysislist <- mclapply(iproanalyses, function (x) { get_feature_to_accession_table(opt = opt, iproanalysis = x) }, mc.cores = appropriatenumcores)
+        names(iproanalysislist) <- iproanalyses
+
+    } else {
+        flog.info("Adding Interproscan analyses signatures to featuredata. Please be patient.")
+        #Aggregate accessions serially
+        iproanalysislist <- lapply(iproanalyses, function(x) get_feature_to_accession_table(opt = opt, iproanalysis = x))
+        names(iproanalysislist) <- iproanalyses
+    }
+
+    for (iproanalysis in names(iproanalysislist)){
+        opt$featuredata <- left_join(opt$featuredata, iproanalysislist[[iproanalysis]])
+        opt$featuredata[, iproanalysis] <- as.character(opt$featuredata[, iproanalysis])
+        opt$featuredata[, iproanalysis][is.na(opt$featuredata[, iproanalysis])] <- "none"
+    }
+
+    return(opt)
+}
+
+
+#' add_blast_results_to_featuredata
+#'
+#' JAMSalpha function
+#' @export
+
 add_blast_results_to_featuredata <- function(opt = opt, blastanalyses = NULL){
 
     if (!(is.null(blastanalyses))){
@@ -131,7 +175,7 @@ harvest_functions <- function(opt = opt, noninterproanalyses = c("FeatType", "EC
     }
     interpronumbaseslist <- NULL
     if("interproscanoutput" %in% names(opt)){
-        opt <- add_interpro_to_featuredata(opt = opt, doinparallel = TRUE)
+        opt <- add_interpro_to_featuredata(opt = opt, doinparallel = FALSE)
 
         iproanalyses <- sort(unique(opt$interproscanoutput$Analysis))
         if ("IproAcc" %in% colnames(opt$interproscanoutput)){
