66522937b9ed92d32e97da8b1452a6c1bf3aa0b5
johnmcculloch <john.anthony.mcculloch@gmail.com>
2022-02-13 23:12:09 -0500

    Tentative implementation of MetaBAT2 Binning

---
+++ R/evaluate_metaBAT_bin.R
@@ -0,0 +1,105 @@
+#' evaluate_metaBAT_bin(opt=NULL)
+#'
+#' Calculates the genome completeness and assembly stats for metaBAT bins.
+#' @export
+
+evaluate_metaBAT_bin <- function(opt = NULL){
+
+    flog.info("Evaluating quality of MAGs.")
+
+    ######################################
+    ##   Declare functions that must    ##
+    ## be declared within this funciton ##
+    ######################################
+
+    getN50 <- function(contigs2length){
+        quantile50 <- (sum(as.numeric(contigs2length$Length))) * 0.5
+        N50 <- max(subset(contigs2length, Lcum > quantile50)$Length)
+        return(N50)
+    }
+
+    getL50 <- function(contigs2length){
+        quantile50 <- (sum(as.numeric(contigs2length$Length))) * 0.5
+        L50 <- length(subset(contigs2length, Lcum > quantile50)$Length)
+        return(L50)
+    }
+
+    getN90 <- function(contigs2length){
+        quantile90 <- (sum(as.numeric(contigs2length$Length))) * 0.9
+        N90 <- max(subset(contigs2length, Lcum > quantile90)$Length)
+        return(N90)
+    }
+
+    getcontigs2length <- function(opt = NULL, bininterest = NULL){
+        contigsdata <- opt$contigsdata
+        contigs2length <- subset(contigsdata, contigsdata[ , "MetaBATbin"] == bininterest)
+        contigs2length <- contigs2length[, c("MetaBATbin", "Contig", "Length", "LKT")]
+        contigs2length <- contigs2length[order(contigs2length$Length, decreasing = TRUE), ]
+        contigs2length$Lcum <- cumsum(contigs2length$Length)
+        return(contigs2length)
+    }
+
+    derive_probable_taxonomy <- function(contigs2length = NULL){
+        N50 <- getN50(contigs2length)
+        LKT_table <- table(subset(contigs2length, Length >= N50)[]$LKT)
+        LKT_table <- LKT_table[order(LKT_table, decreasing = TRUE)]
+        MPT <- names(LKT_table)[1]
+
+        return(MPT)
+    }
+
+    find16SrRNA <- function(contigs2length = NULL, opt = NULL){
+        featuredata <- opt$featuredata
+        #Find how many 16S rRNAs which have been classified can be found in the contigs were looking at.
+        num16S <- length(which((subset(featuredata, Contig %in% contigs2length$Contig)[]$SixteenSid) != "none"))
+
+        return(num16S)
+    }
+
+    get_assembly_stats_by_bin <- function(opt = NULL, bininterest = NULL){
+        contigs2length <- getcontigs2length(opt=opt, bininterest = bininterest)
+        assemblystats <- data.frame(MAGbin = bininterest, MAG_MPT = derive_probable_taxonomy(contigs2length), NumContigs = nrow(contigs2length), ContigSum = sum(contigs2length$Length), LargestContigSize = max(contigs2length$Length), N50 = getN50(contigs2length), L50 = getL50(contigs2length), N90 = getN90(contigs2length), Num16S = find16SrRNA(contigs2length = contigs2length, opt = opt))
+
+        taxinteresttype <- gsub("^LKT__", "", assemblystats$MAG_MPT)
+        expgensize <- as.integer(JAMSMedian_Genome_Sizes[taxinteresttype, "Genome_Size_Median"])
+        if ((length(expgensize) < 1) | (is.na(expgensize)) | (taxinteresttype %in% c("Unclassified", "Missing", "Unclassified__Unclassified"))){
+            expgensize <- as.integer(JAMSMedian_Genome_Sizes["k__Bacteria", "Genome_Size_Median"])
+        }
+        probnumgen <- round((sum(contigs2length$Length) / expgensize), 2)
+        assemblystats$ProbNumGenomes <- probnumgen
+
+        return(assemblystats)
+    }
+
+    get_assembly_stats_for_MAGs <- function(opt = NULL){
+        contigsdata <- opt$contigsdata
+        #Get a vector of bin names
+        named_bins <- unique(opt$contigsdata$MetaBATbin)
+        named_bins <- named_bins[which(named_bins != "none")]
+
+        assemblystats_MAGs <- lapply(named_bins, function(t) { get_assembly_stats_by_bin(opt = opt, bininterest = t) })
+        assemblystats_MAGs <- plyr::ldply(assemblystats_MAGs, rbind)
+        assemblystats_MAGs <- assemblystats_MAGs[order(assemblystats_MAGs$ProbNumGenomes, decreasing = TRUE), ]
+
+        return(assemblystats_MAGs)
+    }
+
+    #Load expected genome size data
+    JAMSMedian_Genome_Sizes_file <- file.path(opt$workingkrakendb, "JAMSMedian_Genome_Sizes.rda")
+    if (file.exists(JAMSMedian_Genome_Sizes_file)){
+        load(JAMSMedian_Genome_Sizes_file)
+    } else {
+        if (file.exists(file.path(opt$workingkrakendb, "JAMS_Median_Genome_Sizes.tsv"))){
+            JAMSMedian_Genome_Sizes <- fread(file.path(opt$workingkrakendb, "JAMS_Median_Genome_Sizes.tsv"), data.table = FALSE, header = TRUE, stringsAsFactors = FALSE)
+        } else {
+            #Fall back on generic taxonomy table and warn user
+            flog.info("JAMS Median Genome Sizes table not found. Falling back on generic JAMS Median Genome Sizes table.")
+            data(JAMSMedian_Genome_Sizes)
+        }
+    }
+    rownames(JAMSMedian_Genome_Sizes) <- JAMSMedian_Genome_Sizes$Taxon
+
+    opt$assemblystats_MAGs <- get_assembly_stats_for_MAGs(opt=opt)
+
+    return(opt)
+}
--- R/get_contig_coverage.R
+++ R/get_contig_coverage.R
@@ -286,5 +286,52 @@ get_contig_coverage <- function(opt = NULL, markduplicates = FALSE, align_as_unp
         opt$LKTdose[is.na(opt$LKTdose)] <- 0
     }
 
+    #Do MetaBAT if applicable
+    if (opt$bin_contigs_with_metabat){
+        setwd(opt$covdir)
+        flog.info("Binning contigs from assembly with metaBAT2")
+        system(paste("jgi_summarize_bam_contig_depths --outputDepth depth.txt", sortedbamfile))
+        system("metabat2 -i contigscov.fa -a depth.txt -o bins_dir/MAGbin")
+        flog.info("Integrating binning information into JAMS")
+        MAGbin_files <- list.files("bins_dir", pattern = "fa$")
+
+        get_contigs_in_bin <- function(bin_file = NULL){
+            bin_contigs <- NULL
+            if (file.exists(file.path("bins_dir", bin_file))){
+                bin_contigs <- system2("grep", args = c("\'^>\'", file.path("bins_dir", bin_file)), stdout = TRUE, stderr = FALSE)
+                bin_contigs <- gsub("^>", "", bin_contigs)
+            }
+            if (length(bin_contigs) < 1){
+                flog.warn(paste("MetaBAT2 bin file", bin_file, "not found, or empty."))
+            }
+
+            return(bin_contigs)
+        }
+
+        bin_contigs_list <- lapply(MAGbin_files, function(x) { get_contigs_in_bin(bin_file = x) } )
+        names(bin_contigs_list) <- MAGbin_files
+
+        opt$contigsdata$MetaBATbin <- 0
+
+        for (mbl in 1:length(bin_contigs_list)){
+            bin_number <- as.numeric(unlist(strsplit(names(bin_contigs_list)[mbl], split = "\\."))[2])
+            curr_contigs <- bin_contigs_list[[mbl]]
+            opt$contigsdata[which(opt$contigsdata$Contig %in% curr_contigs), "MetaBATbin"] <- bin_number
+        }
+
+        #Standardise number of charaters
+        opt$contigsdata$MetaBATbin <- formatC(opt$contigsdata$MetaBATbin, width = nchar(max(opt$contigsdata$MetaBATbin)), flag = "0")
+        opt$contigsdata$MetaBATbin[which(as.numeric(opt$contigsdata$MetaBATbin) == 0)] <- "none"
+        opt$contigsdata$MetaBATbin[which(opt$contigsdata$MetaBATbin != "none")] <- paste(opt$prefix, "MAGbin", opt$contigsdata$MetaBATbin[which(opt$contigsdata$MetaBATbin != "none")], sep = "_")
+
+        opt$MAGdose <- aggregate(NumBases ~ MetaBATbin, FUN = sum, data = opt$contigsdata)
+
+        #Get assemblystats for MAGs
+        if (length(unique(opt$contigsdata$MetaBATbin[which(opt$contigsdata$MetaBATbin != "none")])) > 0){
+            opt <- evaluate_metaBAT_bin(opt = opt)
+        }
+
+    }
+
     return(opt)
 }
