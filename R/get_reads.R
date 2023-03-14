9ac7c52f14338ad51a9d7681e63a272ee07d2211
johnmcculloch <john.anthony.mcculloch@gmail.com>
2023-03-14 16:46:51 -0400

    Moved to fasterq-dump instead of fastq-dump

--- R/get_reads.R
+++ R/get_reads.R
@@ -33,7 +33,8 @@ get_reads <- function(opt = NULL){
         #    flog.info("You are probably on Biowulf. Will use NIH HPC version of SRA toolkit to avoid caching issues.")
         #    commandtorun <- paste(file.path(opt$bindir, "getreadsSRAonBW.sh"), opt$sraaccession, sep =  " ")
         #} else {
-            commandtorun <- paste("fastq-dump --skip-technical --readids --read-filter pass --dumpbase --split-e --clip", opt$sraaccession, sep = " ")
+            #commandtorun <- paste("fastq-dump --skip-technical --readids --read-filter pass --dumpbase --split-e --clip", opt$sraaccession, sep = " ")
+            commandtorun <- paste("fasterq-dump --split-files --skip-technical --threads", opt$threads, opt$sraaccession, sep = " ")
         #}
         system(commandtorun)
 
