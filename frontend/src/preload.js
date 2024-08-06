b9d2bbb0b494de5fb6e4c1703f786ed2b918d024
Tom Mossington <tmossington@gmail.com>
2024-08-06 15:18:22 -0400

    Changed contextBridge to expose the ipcRenderer methods to the renderer process

--- frontend/src/preload.js
+++ frontend/src/preload.js
@@ -1,6 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
+  send: (channel, data) => ipcRenderer.send(channel, data),
+  on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
   loadRDataFile: (filePath) => ipcRenderer.invoke('load-rdata-file', filePath),
   runRScript: (params) => ipcRenderer.invoke('run-r-script', params),
   onParamStr: (callback) => ipcRenderer.on('param-str', (event, paramStr) => callback(paramStr))
