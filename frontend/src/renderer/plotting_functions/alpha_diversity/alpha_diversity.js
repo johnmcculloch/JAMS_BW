6ab090a3d44b3d4941584985a6702e6564402659
Tom Mossington <tmossington@gmail.com>
2024-08-12 15:24:39 -0400

    Initialized user parameter options

--- frontend/src/renderer/plotting_functions/alpha_diversity/alpha_diversity.js
+++ frontend/src/renderer/plotting_functions/alpha_diversity/alpha_diversity.js
@@ -1,6 +1,6 @@
 // alpha_diversity.js
 
 document.getElementById('home-btn').addEventListener('click', () => {
-    // send IPC message to main process to navigate to heatmap page
+    // send IPC message to main process to navigate to home page
     window.electron.send('navigate-to', 'renderer/home/home.html');
 });
