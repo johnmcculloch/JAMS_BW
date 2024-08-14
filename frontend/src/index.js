fb1eccf1e79ec14ccdba04ac7cedcf4ae7cf73b6
Tom Mossington <tmossington@gmail.com>
2024-08-14 15:33:18 -0400

    Initialized index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Create this component
+import './renderer/shared/styles/index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
