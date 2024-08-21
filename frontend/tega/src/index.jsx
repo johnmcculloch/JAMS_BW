a9430a6284bda3fa0a8b0683779778fdf9cc2e88
Tom Mossington <tmossington@gmail.com>
2024-08-21 13:37:18 -0400

    Initialized react/electron app

---
+++ frontend/tega/src/index.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import Home from './components/Home.jsx';
+
+const root = createRoot(document.body);
+root.render(<Home />);
