e06429692a29b7f513a8ab94c1d96b684aa3b448
Tom Mossington <tmossington@gmail.com>
2024-08-21 16:38:25 -0400

    Changed createRoot to getElementById

--- frontend/tega/src/index.jsx
+++ frontend/tega/src/index.jsx
@@ -2,5 +2,5 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import Home from './components/Home.jsx';
 
-const root = createRoot(document.body);
+const root = createRoot(document.getElementById('root'));
 root.render(<Home />);
