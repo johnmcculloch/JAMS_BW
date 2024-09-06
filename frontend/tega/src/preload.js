7772e5aebfb5995a594bb094e5d609b650554b38
Tom Mossington <tmossington@gmail.com>
2024-09-06 14:02:48 -0400

    Added bridging functionality

--- frontend/tega/src/preload.js
+++ frontend/tega/src/preload.js
@@ -1,2 +1,6 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electron', {
+  send: (channel, data) => ipcRenderer.send(channel, data),
+  receive: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args))
+});
