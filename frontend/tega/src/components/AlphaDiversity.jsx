125bbbc1c6eb572ce3b7e020349074f4a8f9b7ef
Tom Mossington <tmossington@gmail.com>
2024-09-10 15:00:50 -0400

    Added logic for handling a file upload, running the AlphaDiversity script, handling the object, and downloading the pdf

--- frontend/tega/src/components/AlphaDiversity.jsx
+++ frontend/tega/src/components/AlphaDiversity.jsx
@@ -30,6 +30,7 @@ const AlphaDiversity = () => {
         class_to_ignore: 'N_A',
     });
     
+    const [alphadiversityData, setAlphaDiversityData] = useState(null);
     const [objects, setObjects] = useState([]);
     const [filePath, setFilePath] = useState('');
     const [selectedObj, setSelectedObj] = useState('');
@@ -42,9 +43,67 @@ const AlphaDiversity = () => {
         });
     };
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log('Selected ExpObj:', selectedObj);
+        try {
+            // Combine the parameters with selected objects and file path
+            const params = {
+                filePath,
+                ExpObj: selectedObj,
+                advancedSettings: {},
+                ...parameters
+            };
+
+            // Call IPC Method to run AlphaDiversity script
+            const result = await window.electron.runAlphaDiversityScript(params) // Verify
+
+            // Update the AD data with result
+            setAlphaDiversityData(result);
+        } catch (error) {
+            console.error("Error generating ordination plot:", error);
+        }
+    };
+
+    const handleFileUpload = async () => {
+        try {
+            const filePath = await window.electron.invoke('open-file-dialog');
+            if (filePath) {
+                setFilePath(filePath);
+                const result = await window.electron.invoke('load-rdata-file', filePath);
+                setObjects(result);
+
+                // Set default selection to the first object, otherwise it will not default to any
+                if (result.length > 0) {
+                    setSelectedObj(result[0]);
+                }
+            }
+        } catch (error) {
+            console.error('Error opening file dialog:', error);
+        }
+    };
+
+    const handleObjSelect = (e) => {
+        const value = e.target.value;
+        setSelectedObj(value);
+        console.log('Selected object state:', value);
+    };
+
     const handleClick = () => {
         window.electron.send('navigate-to', 'home');
     };
+
+    const handleDownloadClick = () => {
+        // Send IPC event to open the alphadiversity PDF
+        window.electron.send('open-alphadiversity-location');
+    };
+
+    useEffect(() => {
+        // Set default selection if objects array changes
+        if (objects.length > 0 && !selectedObj) {
+            setSelectedObj(objects[0]);
+        }
+    }, [objects, selectedObj]);
 
     return (
         <div>
@@ -53,6 +112,7 @@ const AlphaDiversity = () => {
                     Go Back to Home Page
                 </button>
             </div>
+
         </div>
     );
 };
