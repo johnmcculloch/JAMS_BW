c45d101a78635a012770fcdf55001e8bcf7a8a51
Tom Mossington <tmossington@gmail.com>
2025-01-10 15:00:56 -0500

    Updated rendering

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './styles/App.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <App handleNavigateTo={(page) => console.log(`Navigate to ${page}`)} />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
