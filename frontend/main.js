7832b0310f0a929493199a269d376c2755b82377
Tom Mossington <tmossington@gmail.com>
2025-01-10 17:09:51 -0500

    Added imports

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog, shell } = require('electron');
 const path = require('path');
 
 console.log(`app.isPackaged: ${app.isPackaged}`); // Add this line for debugging
