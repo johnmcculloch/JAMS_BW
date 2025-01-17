4a73e3f68a39c95c570b91d8a0603740ae57760f
Tom Mossington <tmossington@gmail.com>
2025-01-17 15:43:20 -0500

    Defined Ordination function

--- frontend/src/components/Ordination.jsx
+++ frontend/src/components/Ordination.jsx
@@ -1,3 +1,9 @@
 import React, {useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+const Ordination = ({ handleNavigateTo }) => {
+    const [parameters, setParameters] = useState({
+        
+    })
+}
