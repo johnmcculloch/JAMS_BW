2cab875af8deb2acb07ff3425aecd879c9b63f58
Tom Mossington <tmossington@gmail.com>
2025-01-17 14:12:43 -0500

    Updated preload commands

--- frontend/preload.js
+++ frontend/preload.js
@@ -1,10 +1,13 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
+const { contextBridge, ipcRenderer } = require('electron');
 
-  for (const dependency of ['chrome', 'node', 'electron']) {
-    replaceText(`${dependency}-version`, process.versions[dependency]);
-  }
+contextBridge.exposeInMainWorld('electron', {
+  send: (channel, data) => ipcRenderer.send(channel, data),
+  receive: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
+  loadRDataFile: (filePath) => ipcRenderer.invoke('load-rdata-file', filePath),
+  invoke: (channel, data) => ipcRenderer.invoke(channel, data),
+  runHeatmapScript: (params) => ipcRenderer.invoke('run-heatmap-script', params),
+  runOrdinationScript: (params) => ipcRenderer.invoke('run-ordination-script', params),
+  runAlphaDiversityScript: (params) => ipcRenderer.invoke('run-alphaDiversity-script', params),
+  runRelabundFeaturesScript: (params) => ipcRenderer.invoke('run-relabundFeatures-script', params),
+  onParamStr: (callback) => ipcRenderer.on('param-str', (event, paramStr) => callback(paramStr))
 });
