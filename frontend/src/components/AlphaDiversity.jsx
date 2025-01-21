affef2ac75fa85347a86ed137b857b668bc7a876
Thomas Mossington <tmossington@gmail.com>
2025-01-21 14:43:44 -0500

    Add initial state and parameters to AlphaDiversity component
    -Defined AlphaDiversity functional component with handleNavigateTo prop
    - Initialized state with useState to manage alpha diversity parameters
    - Added various parameters for alpha diversity analysis
    - Set default values for each parameter

--- frontend/src/components/AlphaDiversity.jsx
+++ frontend/src/components/AlphaDiversity.jsx
@@ -1,3 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+const AlphaDiversity =({ handleNavigateTo }) => {
+    const [parameters, setParameters] = useState({
+        measures: 'c("Observed", "Chao1", "Shannon", "Simpson", "InvSimpson", "GeneCount")',
+        stratify_by_kingdoms: true,
+        glomby: '',
+        samplesToKeep: '',
+        subsetby: '',
+        compareby: '',
+        compareby_order: '',
+        colourby: '',
+        shapeby: '',
+        fillby: '',
+        pairby: '',
+        connectby: '',
+        facetby: '',
+        wrap_facet: '',
+        overlay_boxplot: false,
+        applyfilters: 'none',
+        featcutoff: '',
+        GenomeCompletenessCutoff: '',
+        PPM_normalize_to_bases_sequenced: false,
+        addtit: '',
+        signiflabel: 'p.format',
+        max_pairwise_cats: '4',
+        ignoreunclassified: true,
+        class_to_ignore: 'N_A',
+    });
+    
+}
