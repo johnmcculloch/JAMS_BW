fc2c4e7bb7e37ef1607999cbce29a97443cd162b
Thomas Mossington <tmossington@gmail.com>
2025-08-06 14:49:09 -0400

    refactor: add error handling to RAM detection functions

--- R/detect_hardware.R
+++ R/detect_hardware.R
@@ -19,15 +19,26 @@ get_system_ram <- function() {
 # Detect RAM
 get_linux_ram <- function() {
     ram_kb <- system("awk '/MemTotal/ {print $2}' /proc/meminfo", intern = TRUE)
-    return(as.numeric(ram_kb) * 1024L)
+    ram_kb_numeric <- suppressWarnings(as.numeric(ram_kb))
+
+    # Error handling
+    if (is.na(ram_kb_numeric) || ram_kb_numeric <= 0) {
+        stop("Failed to detect Linux RAM. Please report this at https://github.com/johnmcculloch/JAMS_BW/issues")
+    }
+    return(ram_kb_numeric * 1024L)
 }
 
 get_macos_ram <- function() {
     ram_bytes <- system("sysctl -n hw.memsize", intern = TRUE)
-    return(as.numeric(ram_bytes))
+    ram_bytes_numeric <- suppressWarnings(as.numeric(ram_bytes))
+
+    # Error handling
+    if (is.na(ram_bytes_numeric) || ram_bytes_numeric <= 0) {
+        stop("Failed to detect macOS RAM. Please report this at https://github.com/johnmcculloch/JAMS_BW/issues")
+    }
+    return(ram_bytes_numeric)
 }
 
 get_windows_ram <- function() {
     stop("Windows functionality will be coming shortly! Apologies for the delay, and please contact the developers to use JAMS on windows. (https://github.com/johnmcculloch/JAMS_BW)")
 }
-
