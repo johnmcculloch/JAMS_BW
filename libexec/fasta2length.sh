d7832048d8742260714a8a1f0b8ebcefe7ce65ac
johnmcculloch <john.anthony.mcculloch@gmail.com>
2025-09-22 17:04:41 -0400

    Cleanup or unused functions

--- libexec/fasta2length.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Prepares fastq reads for classifying with kraken
-#For use with JAMSalpha
-#Input is a fasta file. Output is a TSV of sequence names and their length.
-cat "${@}" | awk '$0 ~ ">" {print c; c=0;printf substr($0,2,100) "\t"; } $0 !~ ">" {c+=length($0);} END { print c; }'
