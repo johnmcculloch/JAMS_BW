1d0f7f3674eeecf60a6561304c7d6d4416b02d6a
johnmcculloch <john.anthony.mcculloch@gmail.com>
2025-09-30 20:27:18 -0400

    Better way to download GCA accessions

--- R/get_contigs.R
+++ R/get_contigs.R
@@ -18,10 +18,10 @@ get_contigs <- function(opt = NULL){
     }
 
     #find out if contigs are ready or need to be assembled from reads
-    if (!(is.null(opt$contigs))){
+    if (!(is.null(opt$contigsfasta))){
         #copy contigs
         flog.info("Input sequence supplied are contigs.")
-        opt <- prepare_contigs_for_JAMS(opt = opt, fastafile = opt$contigs)
+        opt <- prepare_contigs_for_JAMS(opt = opt, fastafile = opt$contigsfasta)
 
         #Write NHcontigs to the system
         NHcontigsfastaout <- paste(opt$prefix, "contigs.fasta", sep="_")
@@ -34,20 +34,39 @@ get_contigs <- function(opt = NULL){
         }
 
     } else if (!(is.null(opt$assemblyaccession))){
-        flog.info(paste("Will download contigs from NCBI GenBank under accession number", opt$assemblyaccession[1]))
-        assemblytargetname <- opt$assemblyaccession[1]
-        contigsaccession <- get_genomes_NCBI(organisms = "bacteria", outputdir = opt$workdir, assembly_accession = assemblytargetname, ntop = 1, fileformat = "fasta", simulate = FALSE, nobs = FALSE)
-        if (nrow(contigsaccession) < 1){
-            flog.info(paste("A valid entry with accession number", assemblytargetname, "was not found in GenBank. Only bacterial accession numbers are accepted for the time being. Aborting now."))
-            opt$abort <- TRUE
+
+        GCA_accessions <- unlist(strsplit(opt$assemblyaccession, split = ","))
+        flog.info(paste("Will download contigs from NCBI GenBank under accession number(s)", paste0(GCA_accessions, collapse = ", ")))
+        GCA_accession_list <- lapply(GCA_accessions, function(x) { retrieve_GCA_accession_URL(assemblytargetname = x) } )
+        names(GCA_accession_list) <- GCA_accessions
+        GCA_accession_list <- GCA_accession_list[which(sapply(1:length(GCA_accession_list), function (x) { !is.null(GCA_accession_list[[x]]) }))]
+
+        if (length(GCA_accession_list) == 0){
+
+            #Everything went t!t$ up.
+            flog.warn("FATAL ERROR. None of the NCBI GenBank accessions could be retrieved. Check accession numbers and that you have esearch installed on your system. Aborting now.")
+            IO_jams_workspace_image(opt = opt, operation = "save", verbose = FALSE)
+            q()
+
+        } else {
+
+            #Get list of GCA survivors
+            GCA_accessions <- names(GCA_accession_list)
+            flog.info(paste("Found", paste0(GCA_accessions, collapse = ", ")))
+            #Download, extract and concatenate into a single multifasta file.
+            GCA_accessions_df <- data.frame(Assembly_accession = unlist(GCA_accession_list[GCA_accessions]), GCAfn = paste(GCA_accessions, "fasta.gz", sep = "_"))
+            for (an in 1:nrow(GCA_accessions_df)){
+                #Download
+                tryCatch(download.file(GCA_accessions_df[an, "Assembly_accession"], destfile = GCA_accessions_df[an, "GCAfn"] ), error = function(e) print(paste("Unable to download", GCA_accessions_df[an, "Assembly_accession"])))
+                #Concatenate
+                system(paste("cat", GCA_accessions_df[an, "GCAfn"], ">>", paste0(opt$prefix, "_contigs.fasta.gz")))
+            }
+            #Expand
+            system(paste("unpigz", paste0(opt$prefix, "_contigs.fasta.gz")))
         }
-        gztn <- paste(assemblytargetname, "fasta", "gz", sep = ".")
-        gbfn <- paste(assemblytargetname, "fasta", sep = ".")
-        file.rename(contigsaccession$destfn, gztn)
-        system(paste("gunzip", gztn))
 
         #Set opt$contigs to NULL in case the user has tried to set that as an input too
-        opt <- prepare_contigs_for_JAMS(opt = opt, fastafile = gbfn)
+        opt <- prepare_contigs_for_JAMS(opt = opt, fastafile = paste0(opt$prefix, "_contigs.fasta"))
 
         #Write NHcontigs to the system
         NHcontigsfastaout <- paste(opt$prefix, "contigs.fasta", sep="_")
