3fbf6a77af543dad49b7736599c619021a22e3f0
johnmcculloch <john.anthony.mcculloch@gmail.com>
2026-03-04 16:12:36 -0500

    Safer way of pivoting wider from sparse matrix, and fixed omission of some taxa.

--- R/retrieve_features_by_taxa.R
+++ R/retrieve_features_by_taxa.R
@@ -3,11 +3,12 @@
 #' Returns a long form data frame of stratification by taxa of the relative abundance or number of bases wanted of functional features in wanted samples, given allfeaturesbytaxa_matrix and allfeaturesbytaxa_index metadata present in a JAMS SummarizedExperiment functional object.
 #' @export
 
-retrieve_features_by_taxa <- function(FuncExpObj = NULL, glomby = NULL, assay_for_matrix = "BaseCounts", wantedfeatures = NULL, wantedsamples = NULL, asPPM = TRUE, PPM_normalize_to_bases_sequenced = FALSE, PPMthreshold = 0){
+retrieve_features_by_taxa <- function(FuncExpObj = NULL, glomby = NULL, assay_for_matrix = "BaseCounts", wantedfeatures = NULL, wantedsamples = NULL, asPPM = TRUE, PPM_normalize_to_bases_sequenced = FALSE, PPMthreshold = 0, append_metatada = TRUE){
 
     if (assay_for_matrix == "GeneCounts"){
         sparsematrix_name <- "allfeaturesbytaxa_GeneCounts_matrix"
         sparsematrix_index_name <- "allfeaturesbytaxa_GeneCounts_index"
+        asPPM <- FALSE
     } else {
         sparsematrix_name <- "allfeaturesbytaxa_matrix"
         sparsematrix_index_name <- "allfeaturesbytaxa_index"
@@ -44,20 +45,20 @@ retrieve_features_by_taxa <- function(FuncExpObj = NULL, glomby = NULL, assay_fo
     }
 
     #Ensure correct class
-    rowsinterestdf$RowNumber <- as.integer(rowsinterestdf$RowNumber)
+    rowsinterestdf$RowNumber <- as.character(rowsinterestdf$RowNumber)
     #Eliminate unavailable information.
     rowsinterestdf <- rowsinterestdf[!is.na(rowsinterestdf$RowNumber), ]
-
-    allfeaturesbytaxa_interest <- as.matrix(metadata(FuncExpObj)[[sparsematrix_name]][rowsinterestdf$RowNumber, , drop = FALSE])
+    #Safer to go by named rows than numeric, as it will throw an error if the name does not exist.
+    allfeaturesbytaxa_interest <- as.matrix(metadata(FuncExpObj)[[sparsematrix_name]][as.character(rowsinterestdf$RowNumber), , drop = FALSE])
     #Prune empty taxa, i.e. taxa which have 0 counts for all of the features of interest
     allfeaturesbytaxa_interest <- as.data.frame(allfeaturesbytaxa_interest[, which(colSums(allfeaturesbytaxa_interest) != 0)])
-    allfeaturesbytaxa_interest$RowNumber <- as.integer(rownames(allfeaturesbytaxa_interest))
+    allfeaturesbytaxa_interest$RowNumber <- as.character(rownames(allfeaturesbytaxa_interest))
     allfeaturesbytaxa_interest <- left_join(allfeaturesbytaxa_interest, rowsinterestdf, by = "RowNumber")
     allfeaturesbytaxa_interest$RowNumber <- NULL
     allfeaturesbytaxa_interest <- allfeaturesbytaxa_interest[ , c("Sample", "Accession", (sort(colnames(allfeaturesbytaxa_interest)[which(!colnames(allfeaturesbytaxa_interest) %in% c("Sample", "Accession"))])))]
 
     #Agglomerate if applicable
-    if (!is.null(glomby)){
+    if ((!is.null(glomby) && (glomby != "LKT"))){
         #Check this feature is available before proceeding
         if (!JAMS2_object){
             stop("Stratification of functions by taxonomic levels higher than LKT is only available for SummarizedExperiment objects made with JAMS version > 2.1.0 ")
@@ -65,33 +66,35 @@ retrieve_features_by_taxa <- function(FuncExpObj = NULL, glomby = NULL, assay_fo
         #Obtain taxonomy table
         stt <- as.data.frame(metadata(FuncExpObj)$tt)
         LKTs_to_glom <- (sort(colnames(allfeaturesbytaxa_interest)[which(!colnames(allfeaturesbytaxa_interest) %in% c("Sample", "Accession", "Ultra_low_abundance_LKTs"))]))
-        stt <- stt[which(LKTs_to_glom %in% stt$LKT), c(glomby, "LKT")]
+        stt <- stt[which(stt$LKT %in% LKTs_to_glom), c(glomby, "LKT")]
         #Fill in unclassifieds
         stt[which(is.na(stt[ , glomby])), glomby] <- "Unclassified"
         #extract matrix to glom
         LKTs_to_glom_mat <- allfeaturesbytaxa_interest[ , c("Sample", "Accession", LKTs_to_glom)]
         #Filter LKTs present in stt
         common_LKTs <- intersect(colnames(LKTs_to_glom_mat), stt$LKT)
-        #Keep only those LKT columns
-        LKTs_filtered <- LKTs_to_glom_mat[ , common_LKTs, drop = FALSE]
+
+        LKTs_filtered <- LKTs_to_glom_mat[ , c("Sample", "Accession", common_LKTs), drop = FALSE]
+        LKTs_filtered$SampleAccession <- paste(LKTs_filtered$Sample, LKTs_filtered$Accession, sep = "-")
+        SAsafeLookup <- LKTs_filtered[ , c("SampleAccession", "Sample", "Accession")]
+        LKTs_filtered$Sample <- NULL
+        LKTs_filtered$Accession <- NULL
+
+        LKTs_filtered <- LKTs_filtered[ , c("SampleAccession", common_LKTs), drop = FALSE]
         # Reshape LKTs_to_glom_mat to long format
-        LKTs_to_glom_long <- LKTs_filtered %>%  mutate(row_id = row_number()) %>%  pivot_longer(cols = -row_id, names_to = "LKT", values_to = "NumBases")
+        LKTs_to_glom_long <- LKTs_filtered %>%  pivot_longer(cols = -SampleAccession, names_to = "LKT", values_to = "NumBases")
         # Join with stt to get glomby level for each LKT
-        LKTs_to_glom_long <- LKTs_to_glom_long %>% left_join(stt, by = "LKT")
+        LKTs_to_glom_long <- left_join(LKTs_to_glom_long, stt, by = "LKT")
         #Rename relevant glomby column to make things more practical
         colnames(LKTs_to_glom_long)[which(colnames(LKTs_to_glom_long) == glomby)] <- "Glomby"
         #Aggregate counts per row_id × Glomby
-        Glomby_sum <- LKTs_to_glom_long %>% group_by(row_id, Glomby) %>% summarise(NumBases = sum(NumBases, na.rm = TRUE), .groups = "drop")
+        Glomby_sum <- LKTs_to_glom_long %>% group_by(SampleAccession, Glomby) %>% summarise(NumBases = sum(NumBases, na.rm = TRUE), .groups = "drop")
         #Spread back to wide format (one row per original row)
         Glomby_wide <- Glomby_sum %>% pivot_wider(names_from = Glomby, values_from = NumBases, values_fill = 0)
         Glomby_wide <- as.data.frame(Glomby_wide)
-        rownames(Glomby_wide) <- Glomby_wide$row_id
-        Glomby_wide$row_id <- NULL
-        Glom_taxa <- colnames(Glomby_wide)
-        Glomby_wide[ , "Sample"] <- allfeaturesbytaxa_interest[rownames(Glomby_wide), "Sample"]
-        Glomby_wide[ , "Accession"] <- allfeaturesbytaxa_interest[rownames(Glomby_wide), "Accession"]
-        #Rearrange order
-        allfeaturesbytaxa_interest <- Glomby_wide[ , c("Sample", "Accession", Glom_taxa)]
+        Glomby_wide <- left_join(SAsafeLookup, Glomby_wide, by = "SampleAccession")
+        Glomby_wide$SampleAccession <- NULL
+        allfeaturesbytaxa_interest <- Glomby_wide
     }
 
     if (asPPM){
@@ -105,20 +108,24 @@ retrieve_features_by_taxa <- function(FuncExpObj = NULL, glomby = NULL, assay_fo
         numbases2sampl <- as.data.frame(t(metadata(FuncExpObj)[[totbases]]))
         numbases2sampl$Sample <- rownames(numbases2sampl)
         taxsplit <- left_join(taxsplit, numbases2sampl, by = "Sample")
-        LKTcolumns <- colnames(taxsplit)[!(colnames(taxsplit) %in% c("Sample", "Accession", "NumBases"))]
+        TaxonColumns <- colnames(taxsplit)[!(colnames(taxsplit) %in% c("Sample", "Accession", "NumBases"))]
 
-        #Transform to PPM
-        for (colm in LKTcolumns){
-            taxsplit[ , colm] <- round(((taxsplit[ , colm] / taxsplit$NumBases) * 1000000), 0)
+        #Transform to PPM. Keep 2 significant decimal places because splitting a 1PPM gene several ways may lead to fractions.
+        for (colm in TaxonColumns){
+            taxsplit[ , colm] <- round(((taxsplit[ , colm] / taxsplit$NumBases) * 1000000), 2)
         }
 
         #Denoise
-        LKTsMaxima <- sapply(LKTcolumns, function(x) {max(taxsplit[ , x])})
-        LKTsToKeep <- names(which(LKTsMaxima > PPMthreshold))
-        sample2metadata <- as.data.frame(colData(FuncExpObj))
-        nonSamplecolms <- colnames(sample2metadata)[colnames(sample2metadata) != "Sample"]
-        taxsplit <- left_join(taxsplit, sample2metadata, by = "Sample")
-        taxsplit <- taxsplit[ , c("Sample", "Accession", "NumBases", nonSamplecolms, LKTsToKeep)]
+        TaxaMaxima <- sapply(TaxonColumns, function(x) {max(taxsplit[ , x])})
+        TaxaToKeep <- names(which(TaxaMaxima > PPMthreshold))
+        if (append_metatada){
+            sample2metadata <- as.data.frame(colData(FuncExpObj))
+            nonSamplecolms <- colnames(sample2metadata)[colnames(sample2metadata) != "Sample"]
+            taxsplit <- left_join(taxsplit, sample2metadata, by = "Sample")
+        } else {
+            nonSamplecolms <- NULL
+        }
+        taxsplit <- taxsplit[ , c("Sample", "Accession", "NumBases", nonSamplecolms, TaxaToKeep)]
         taxsplit$NumBases <- NULL
 
         return(taxsplit)
